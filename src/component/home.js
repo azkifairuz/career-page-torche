e10bac117b987e02f6074daff958bb8dc93eb58e
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-03-07 18:26:09 +0800

    test update

--- src/component/home.js
+++ src/component/home.js
@@ -1,8 +1,8 @@
 function Home() {
-    return(
-        <div className="text-center">
-            <h1>Home</h1>
-        </div>
-    )
+  return (
+      <div className="text-center">
+        <h1>Home</h1>
+      </div>
+  );
 }
-export default Home
+export default Home;
