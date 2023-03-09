f0c3f84aae99f0a01329ac300d6115b6c078533f
azkifairuz <azkiajmal@gmail.com>
2023-03-09 22:48:19 +0700

    Add biodata in dashboard

--- src/components/User/Dashboard.js
+++ src/components/User/Dashboard.js
@@ -38,27 +38,10 @@ function UserDashboard(params) {
                 </button>
                 </div>
             </div>
-            <div className="min-h-screen m-auto min-w-min flex justify-center my-14 md:w-3/4">
-                <div className="TentangSaya  h-fit  rounded-md  shadow-lg flex flex-col w-1/2 items-center  ">
+            <div className=" m-auto w-full md:min-w-full flex-col items-center gap-10 flex justify-center my-14 ">
+                <div className="TentangSaya  h-fit  rounded-md  shadow-lg flex flex-col md:w-1/2 items-center  ">
                 <div className="w-full bg-primary rounded-t-md items-center flex justify-between text-white p-5">
                     <h2>Tentang Saya</h2>
-                    <div className="flex space-x-3">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-6 h-6"
-                    >
-                        <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                        />
-                    </svg>
-                    <p>UBAH</p>
-                    </div>
                 </div>
                 <div className="w-full rounded-b-md p-5 ">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni sunt
@@ -70,7 +53,35 @@ function UserDashboard(params) {
                     ratione.
                 </div>
                 </div>
+                <div className="DataDiri  h-fit  rounded-md  shadow-lg flex flex-col md:w-1/2 items-center  ">
+                <div className="w-full bg-primary rounded-t-md items-center flex justify-between text-white p-5">
+                    <h2>Data Diri</h2>
+                </div>
+                <div className="w-full flex flex-col space-y-5 justify-start rounded-b-md p-5 ">
+                    <div className="text-left">
+                        <h1 className="font-bold ">Nama Lengkap</h1>
+                        <p>John Kebab</p>
+                    </div>
+                    <div className="text-left">
+                        <h1 className="font-bold ">Nama Panggilan</h1>
+                        <p>John</p>
+                    </div>
+                    <div className="text-left">
+                        <h1 className="font-bold ">Jenis Kelamin</h1>
+                        <p>Laki-Laki</p>
+                    </div>
+                    <div className="text-left">
+                        <h1 className="font-bold ">Nomor Telepon</h1>
+                        <p>Laki-Laki</p>
+                    </div>
+                    <div className="text-left">
+                        <h1 className="font-bold ">Lokasi Saat Ini</h1>
+                        <p>Laki-Laki</p>
+                    </div>
+                </div>
+                </div>
             </div>
+            
         </div>
     );
 }
