f2f5aaa973af64c18f769a77b00d8170389d9481
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-03-11 07:57:44 +0800

    add theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors:{
-        'primary': '#0b122a',
-        'darkGrey': '#444444',
-      }
+      colors: {
+        primary: "#0b122a",
+        darkGrey: "#444444",
+      },
+      fontFamily: {
+        rajdhani: ["Rajdhani", "sans-serif"],
+        cairo: ["Cairo", "sans-serif"],
+      },
     },
-    
   },
   plugins: [],
-}
+};
