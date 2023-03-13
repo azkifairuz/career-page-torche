777a5b7929a6b2ce36ffaaccc74cd3b8f9dd737a
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-03-13 20:47:50 +0800

    fix null count display

--- src/components/Admin/TabButton.js
+++ src/components/Admin/TabButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function TabButton(props) {
-  const { title, count, active } = props;
+  const { title, count = null, active } = props;
   return (
     <button
       className={`flex items-center gap-2 p-4 border-b-2 border-transparent ${
@@ -9,9 +9,9 @@ export default function TabButton(props) {
       }`}
     >
       <h1 className="font-semibold font-rajdhani ">{title}</h1>
-      <p className="text-xs rounded font-semibold bg-white p-[6px] py-[0.5px] text-blue-800">
+      {count !== null && <p className="text-xs rounded font-semibold bg-white p-[6px] py-[0.5px] text-blue-800">
         {count}
-      </p>
+      </p>}
     </button>
   );
 }
