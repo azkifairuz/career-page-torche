0bc764f11675e261beaa3bf9bb10c75fc57f222a
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-03-13 20:09:09 +0800

    update table column layout

--- src/components/Admin/Table/index.js
+++ src/components/Admin/Table/index.js
@@ -3,14 +3,14 @@ import Card from "./Card";
 
 export default function Table() {
   return (
-    <table className="w-full">
-      <tr className="bg-slate-200 shadow-md ">
-        <th className="py-3 px-6 text-sm " align="left">
+    <table className="w-full ">
+      <tr className="bg-slate-200 shadow-md border-spacing-0">
+        <th className="py-3 pl-6 text-sm" align="left">
           Lowongan
         </th>
         <th className="text-sm">Jumlah Pelamar per Status</th>
         <th className="text-sm">Ganti Status</th>
-        <th className="text-sm">Aksi</th>
+        <th className="text-sm pr-4">Aksi</th>
       </tr>
       <Card />
     </table>
