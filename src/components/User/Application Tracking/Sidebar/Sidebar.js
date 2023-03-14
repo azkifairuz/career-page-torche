8763cf0c33b12449645ec3d9c878cbd071984199
azkifairuz <azkiajmal@gmail.com>
2023-03-14 13:35:08 +0700

    create item for sidebar

--- src/components/User/Application Tracking/Sidebar/Sidebar.js
+++ src/components/User/Application Tracking/Sidebar/Sidebar.js
@@ -1,12 +1,45 @@
 import Logo from "../../../../assets/logos/Torche_Logo-01_White.png";
 import * as Icon from "react-bootstrap-icons";
+import Item from "./item";
 export default function Sidebar(params) {
-    return(
-        <aside className="flex flex-col w-[20vw] text-white bg-primary sticky top-0 left-0 h-screen py-8 px-6">
-            <header className="cursor-pointer flex gap-2 items-center">
-                <Icon.ChevronLeft size={20} className=""/>
-                <h1 className="text-2xl font-rajdhani font-bold">Profil</h1>
-            </header>
-        </aside>
-    )
+  return (
+    <aside className="flex flex-col w-[20vw] text-white bg-primary sticky top-0 left-0 h-screen py-8 px-6">
+      <header className="cursor-pointer flex gap-2 items-center">
+        <Icon.ChevronLeft size={20} className="" />
+        <h1 className="text-2xl font-rajdhani font-bold">Profil</h1>
+      </header>
+      <main className="flex flex-col justify-between h-full">
+        <ul className="flex flex-col gap-2 mt-14">
+        <Item
+            title="Semua"
+            href="semua"
+          />
+        <Item
+            title="Terkirim"
+            href="terkirim"
+          />
+        <Item
+            title="Dilihat HRD"
+            href="dilihat"
+          />
+        <Item
+            title="Diproses"
+            href="diproses"
+          />
+        <Item
+            title="Diterima kerja"
+            href="diterima kerja"
+          />
+        <Item
+            title="Ditolak"
+            href="ditolak"
+          />
+        <Item
+            title="Belum Terkirim"
+            href="belum"
+          />
+        </ul>
+      </main>
+    </aside>
+  );
 }
---
+++ src/components/User/Application Tracking/Sidebar/item.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Item(props) {
+  const { title, href } = props;
+  return (
+    <Link
+      to={href}
+      className="flex gap-6 items-center px-4 py-3 hover:bg-slate-500 rounded-lg font-rajdhani font-medium"
+    >
+      
+      <span>{title}</span>
+    </Link>
+  );
+}
