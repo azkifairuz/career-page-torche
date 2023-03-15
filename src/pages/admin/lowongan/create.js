1b7ff71ad7d2acc269cfd4ae7575005afffe5bba
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-03-15 21:27:02 +0800

    create lowongan page

--- src/pages/admin/lowongan/create.js
+++ src/pages/admin/lowongan/create.js
@@ -18,7 +18,7 @@ export default function Create() {
   });
 
   return (
-    <main className="p-8 bg-slate-200 flex-1">
+    <main className="p-8 bg-slate-200 flex-1 flex flex-col gap-4">
       <h1>Buat lowongan baru</h1>
       <section className="rounded-xl bg-white p-4">
         <h1>Informasi Utama</h1>
@@ -31,9 +31,75 @@ export default function Create() {
         </div>
         <div className="flex flex-col">
           <label>Deskripsi</label>
-          <textarea></textarea>
+          <textarea value={lowongan.deskripsi}></textarea>
         </div>
       </section>
+      <seciton className="flex flex-col rounded-xl bg-white p-4">
+        <h1>Lokasi pekerjaan</h1>
+        <p>Secara bertahap kami belaksdjklajklsd</p>
+        <div className="flex gap-4">
+          <select className="flex-1">
+            <option>Provinsi</option>
+          </select>
+          <select className="flex-1">
+            <option>Kota</option>
+          </select>
+          <select className="flex-1">
+            <option>Kecamatan</option>
+          </select>
+        </div>
+      </seciton>
+      <seciton className="flex flex-col rounded-xl bg-white p-4">
+        <h1>Waktu Kerja </h1>
+        <div className="flex gap-4">
+          <input type="time" className="flex-1 p-4" />
+          <input type="time" className="flex-1 p-4" />
+        </div>
+        <p>Pilih hari kerja di bawah sesuai kebutuhan anda</p>
+        <div className="flex gap-2">
+          <input type="checkbox" />
+          <label>Minggu</label>
+          <input type="checkbox" />
+          <label>Senin</label>
+          <input type="checkbox" />
+          <label>Selasa</label>
+          <input type="checkbox" />
+          <label>Rabu</label>
+          <input type="checkbox" />
+          <label>Kamis</label>
+          <input type="checkbox" />
+          <label>Jumat</label>
+          <input type="checkbox" />
+          <label>Sabtu</label>
+        </div>
+      </seciton>
+      <seciton className="flex flex-col rounded-xl bg-white p-4">
+        <h1>Informasi pelengkap</h1>
+        <div className="flex gap-4">
+          <select className="flex-1">
+            <option>Provinsi</option>
+          </select>
+          <select className="flex-1">
+            <option>Kota</option>
+          </select>
+          <select className="flex-1">
+            <option>Kecamatan</option>
+          </select>
+        </div>
+        <p>Agar lebih menarik minat para pelamar kljasdkljakldjsad</p>
+        <div className="flex gap-4">
+          <input
+            type="number"
+            className="flex-1 p-4"
+            placeholder="isi gaji minimal di sini"
+          />
+          <input
+            type="number"
+            className="flex-1 p-4"
+            placeholder="isi gaji maksimal di sini"
+          />
+        </div>
+      </seciton>
     </main>
   );
 }
