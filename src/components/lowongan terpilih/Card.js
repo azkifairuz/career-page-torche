0ddfd33c2ab72601c2dd1ac795dbc300b0732d20
azkifairuz <azkiajmal@gmail.com>
2023-03-16 01:00:46 +0700

    create dynamic card

--- src/components/lowongan terpilih/Card.js
+++ src/components/lowongan terpilih/Card.js
@@ -1,16 +1,17 @@
 import * as Icon from "react-bootstrap-icons"
 export default function LokerCard(props) {
+    const {jobTitle,jobType} = props
     return(
         <div className="border flex flex-col justify-between  shadow rounded-lg p-4  md:w-[20vw] h-52 m-2 ">
             <div className="flex flex-col gap-3">
-                <h1 className="text-2xl font-rajdhani mt-4 font-bold">JobTitle</h1>
+                <h1 className="text-2xl font-rajdhani mt-4 font-bold">{jobTitle}</h1>
                 <div className="flex items-center gap-3">
                     <Icon.Circle/>
                     <p className="font-cairo">Job Place</p>
                 </div>
                 <div className="flex items-center gap-3">
                     <Icon.Clock color="blue"/>
-                    <p className="font-cairo">Job type</p>
+                    <p className="font-cairo">{jobType}</p>
                 </div>
             </div>
             <button className="w-full py-2 px-4 bg-blue-500 text-white">Lihat</button>
--- src/data/loker.js
+++ src/data/loker.js
@@ -23,4 +23,76 @@ export const lokers = [
         'CompanyName':'PT PT an',
         'Deskripsi':'lorem ipsum doler'
     },
+    {
+        id:1,
+    'JobTitle':'web dev1',
+    'CompanyName':'PT PT an',
+    'Deskripsi':'lorem ipsum doler'
+    },
+    {
+        id:2,
+        'JobTitle':'web dev2',
+        'CompanyName':'PT PT an',
+        'Deskripsi':'lorem ipsum doler'
+    },
+    {
+        id:3,
+        'JobTitle':'web dev3',
+        'CompanyName':'PT PT an',
+        'Deskripsi':'lorem ipsum doler'
+    },
+    {
+        id:4,
+        'JobTitle':'web dev4',
+        'CompanyName':'PT PT an',
+        'Deskripsi':'lorem ipsum doler'
+    },
+    {
+        id:1,
+    'JobTitle':'web dev1',
+    'CompanyName':'PT PT an',
+    'Deskripsi':'lorem ipsum doler'
+    },
+    {
+        id:2,
+        'JobTitle':'web dev2',
+        'CompanyName':'PT PT an',
+        'Deskripsi':'lorem ipsum doler'
+    },
+    {
+        id:3,
+        'JobTitle':'web dev3',
+        'CompanyName':'PT PT an',
+        'Deskripsi':'lorem ipsum doler'
+    },
+    {
+        id:4,
+        'JobTitle':'web dev4',
+        'CompanyName':'PT PT an',
+        'Deskripsi':'lorem ipsum doler'
+    },
+    {
+        id:1,
+    'JobTitle':'web dev1',
+    'CompanyName':'PT PT an',
+    'Deskripsi':'lorem ipsum doler'
+    },
+    {
+        id:2,
+        'JobTitle':'web dev2',
+        'CompanyName':'PT PT an',
+        'Deskripsi':'lorem ipsum doler'
+    },
+    {
+        id:3,
+        'JobTitle':'web dev3',
+        'CompanyName':'PT PT an',
+        'Deskripsi':'lorem ipsum doler'
+    },
+    {
+        id:4,
+        'JobTitle':'web dev4',
+        'CompanyName':'PT PT an',
+        'Deskripsi':'lorem ipsum doler'
+    },
 ]
