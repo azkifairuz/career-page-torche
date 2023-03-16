18c037c4c9de0ab3a314bb0baea585b1570cba0e
azkifairuz <azkiajmal@gmail.com>
2023-03-16 15:07:01 +0700

    fix padding

--- src/components/category/index.js
+++ src/components/category/index.js
@@ -4,7 +4,7 @@ export default function Category() {
     return(
         <div className="m-5 p-2">
             <h1 className="font-rajdhani font-bold text-2xl">Cari Berdasarkan Kategori</h1>
-            <div className=" grid grid-cols-4 w-fit mx-auto h-fit p-4 ">
+            <div className=" grid grid-cols-4 w-fit gap-4 mx-auto h-fit p-4 ">
                 {
                     categories.map((category,index)=>{
                         return <CategoryCard ker={index} Category={category.Category} icon={category.icon} jumlah={category.Jumlah} />
