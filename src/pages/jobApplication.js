550aaeb043fedb97c1036c8c3a1d89a6037e19ec
azkifairuz <azkiajmal@gmail.com>
2023-03-17 22:13:30 +0700

    add requirement to pages

--- src/pages/jobApplication.js
+++ src/pages/jobApplication.js
@@ -1,9 +1,11 @@
 import Information from "components/JobApplication/information"
+import Requirement from "components/JobApplication/requirement"
 
 function JobApplication() {
     return (
         <div className="min-h-screen w-1/2 mx-auto bg-gray-300">
             <Information/>
+            <Requirement/>
         </div>
     )
 }
