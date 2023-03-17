217a86e4d121a18883b9558f7355f25e939fe29a
azkifairuz <azkiajmal@gmail.com>
2023-03-17 22:43:26 +0700

    add button lamar

--- src/components/JobApplication/description.js
+++ src/components/JobApplication/description.js
@@ -9,7 +9,7 @@ export default function Description(params) {
                 <CardRequirement title="HTML"/>
             </div>
             <div className="font-rajdhani flex flex-col gap-5 text-md">
-                <p>Deskrii Pekerjaan</p>
+                <p>Deskripsii Pekerjaan</p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, ipsa cum! Culpa quas tenetur vero incidunt eaque, sit accusamus id hic, voluptatum consequuntur reiciendis quo quam ipsa deserunt suscipit. Ipsam fugiat totam molestiae, itaque dolores magni repellat veniam dolor? Iusto, culpa fugit. Incidunt explicabo odit ea beatae, in nemo nostrum?
             </div>
             <div className="flex w-full justify-between">
--- src/pages/jobApplication.js
+++ src/pages/jobApplication.js
@@ -4,10 +4,15 @@ import Requirement from "components/JobApplication/requirement"
 
 function JobApplication() {
     return (
-        <div className="min-h-screen w-1/2 mx-auto flex flex-col gap-4 bg-gray-300">
+        <div className="min-h-screen w-1/2  mx-auto flex flex-col my-2  bg-gray-300">
+            <div className="flex flex-col gap-4">
             <Information/>
             <Requirement/>
             <Description/>
+            </div>
+            <div className="p-2 bg-white">
+            <button className="px-4 w-full text-wxl font-cairo rounded-md  bottom-0 py-2 bg-blue-500 text-white">Lamar</button>
+            </div>
         </div>
     )
 }
