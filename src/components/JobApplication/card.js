b50e0ac40285810a6526efeaf5f5a44339b31ddc
azkifairuz <azkiajmal@gmail.com>
2023-03-17 22:38:11 +0700

    add description to pages

--- src/components/JobApplication/card.js
+++ src/components/JobApplication/card.js
@@ -1,10 +1,10 @@
 import * as Icon from "react-bootstrap-icons"
 export default function CardRequirement(props) {
-        const Title = props
+        
         return(
-            <div className="px-4 py-2 flex font-rajdhani bg-blue-400 text-blue-600">
+            <div className="px-4 py-2 font-bold rounded-sm shadow flex font-rajdhani bg-blue-200 text-blue-600">
                 <Icon.Check size={20} />
-                {Title}
+                {props.title}
             </div>
         )
     
--- src/pages/jobApplication.js
+++ src/pages/jobApplication.js
@@ -1,3 +1,4 @@
+import Description from "components/JobApplication/description"
 import Information from "components/JobApplication/information"
 import Requirement from "components/JobApplication/requirement"
 
@@ -6,6 +7,7 @@ function JobApplication() {
         <div className="min-h-screen w-1/2 mx-auto flex flex-col gap-4 bg-gray-300">
             <Information/>
             <Requirement/>
+            <Description/>
         </div>
     )
 }
