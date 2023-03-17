00b6031478c0e1ab03118a9e178eb4cc5cdb938b
azkifairuz <azkiajmal@gmail.com>
2023-03-17 22:33:04 +0700

    create description section

---
+++ src/components/JobApplication/description.js
@@ -0,0 +1,19 @@
+export default function Description(params) {
+    return(
+        <div className="bg-white p-5 h-fitt w-full flex justify-between">
+            <div className="font-rajdhani text-md">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, ipsa cum! Culpa quas tenetur vero incidunt eaque, sit accusamus id hic, voluptatum consequuntur reiciendis quo quam ipsa deserunt suscipit. Ipsam fugiat totam molestiae, itaque dolores magni repellat veniam dolor? Iusto, culpa fugit. Incidunt explicabo odit ea beatae, in nemo nostrum?
+            </div>
+            <div className="flex w-full justify-between">
+                <div>
+                    <p>Hari Kerja</p>
+                    <h1>Diinfokan Lebih Lanjut</h1>
+                </div>
+                <div>
+                    <p>jam Kerja</p>
+                    <h1>Diinfokan Lebih Lanjut</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
