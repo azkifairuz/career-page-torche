adc06656576589845b40331a957a5af6dc4b9876
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-03-18 22:03:04 +0800

    setting up reducer and context

---
+++ src/context/AuthContext.js
@@ -0,0 +1,32 @@
+import { createContext, useContext, useReducer } from "react";
+
+const AuthStateContext = createContext();
+const AuthDispatchContext = createContext();
+
+export const useAuthState = () => {
+  const context = useContext(AuthStateContext);
+  if (context === undefined) {
+    throw new Error("useAuthState must be used within a AuthProvider");
+  }
+  return context;
+};
+
+export const useAuthDispatch = () => {
+  const context = useContext(AuthDispatchContext);
+  if (context === undefined) {
+    throw new Error("useAuthDispatch must be used within a AuthProvider");
+  }
+  return context;
+};
+
+export const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AuthReducer, initialState);
+
+  return (
+    <AuthStateContext.Provider value={state}>
+      <AuthDispatchContext.Provider value={dispatch}>
+        {children}
+      </AuthDispatchContext.Provider>
+    </AuthStateContext.Provider>
+  );
+};
