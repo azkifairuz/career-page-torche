d0df120764afa25594b58d47aa9a40bdd4656042
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-03-19 19:48:39 +0800

    default file to export all contents

---
+++ src/context/index.js
@@ -0,0 +1,4 @@
+import { useAuthDispatch, useAuthState, AuthProvider } from "./AuthContext";
+import { login, logout } from "./actions";
+
+export { useAuthDispatch, useAuthState, AuthProvider, login, logout };
