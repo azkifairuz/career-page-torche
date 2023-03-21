6c5e1592e5e673e226de35ee1f75b64b86e39701
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-03-21 20:51:53 +0800

    add tailwind font and color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#0b122a",
+        // primary: "#0b122a",
         darkGrey: "#444444",
+        primaryNavy: {
+          main: "#0B122A",
+        },
+        primaryBlue: {
+          main: "#5885E9",
+        },
+        neutral: {
+          100: "#151515",
+        }
       },
       fontFamily: {
         rajdhani: ["Rajdhani", "sans-serif"],
