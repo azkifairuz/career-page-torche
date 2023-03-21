595e9f14008563c13d39e69adc7e22c12df9c27e
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-03-21 20:52:05 +0800

    refactor sign in page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,5 +1,8 @@
+import InputField from "components/atoms/InputField";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import logoBlack from "assets/logos/Torche_Logo_Black.png";
+import logoWhite from "assets/logos/Torche_Logo-01_White.png";
 
 function Login() {
   const navigate = useNavigate();
@@ -16,54 +19,64 @@ function Login() {
   }
 
   return (
-    <main class="flex lg:px-100 md:justify-center lg:justify-end bg-primary min-h-screen">
-      <div class="w-full bg-white rounded-lg shadow shadow-slate-400  md:mt-0 sm:max-w-md xl:p-0 bg-primary-500 border-gray-700">
-        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
-            Sign in
-          </h1>
-          <form class="space-y-4 md:space-y-6" action="" method="post">
-            <div>
-              <label
-                for="username"
-                class="block mb-2 text-sm font-medium text-gray-900 "
-              >
-                Your username
-              </label>
-              <input
-                type="text"
-                value={users}
-                onChange={handler}
-                name="username"
-                id="username"
-                class="bg-primary-800 border border-gray-300  sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                placeholder="Input Username"
-                required
-              />
-            </div>
-            <div>
-              <label for="password" class="block mb-2 text-sm font-medium  ">
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                placeholder="••••••••"
-                class="bg-primary-800 border border-gray-300  sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                required
-              />
-            </div>
-            <button
-              onClick={isAdmin}
-              name="loginbutton"
-              class="w-full  bg-blue-500 text-white hover:bg-primary-100 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-            >
-              Sign in
-            </button>
-          </form>
-        </div>
+    <main className=" flex p-0 md:px-[100px] justify-center lg:justify-end items-center bg-primaryNavy-main min-h-screen">
+      <div className="flex flex-col items-center gap-[24px] p-[16px] md:p-[48px] bg-white font-cairo w-[503px] h-screen md:h-fit">
+        <img
+          src={logoBlack}
+          alt="Logo"
+          className="block md:hidden w-[128px] mt-[36px] mb-[24px]"
+        />
+
+        <form
+          className="flex flex-col gap-[16px] w-full h-full"
+          action=""
+          method="post"
+        >
+          <InputField
+            label="Email"
+            type="text"
+            placeholder="Your Email Here    "
+          />
+          <div className="flex flex-col gap-[8px] items-start   ">
+            <InputField
+              label="Kata Sandi"
+              type="password"
+              placeholder="••••••••"
+            />
+            <a href="/#">
+              <p className="text-sm text-center text-primaryBlue-main font-cairo font-bold">
+                Forgot Password?
+              </p>
+            </a>
+          </div>
+          <button
+            onClick={isAdmin}
+            name="loginbutton"
+            class="w-full bg-primaryBlue-main text-white focus:outline-none px-[14px] py-[16px] rounded-[8px]"
+          >
+            Masuk
+          </button>
+        </form>
+        <p className="text-neutral-100 text-[14px]">atau masuk dengan </p>
+        <button
+          onClick={isAdmin}
+          name="loginbutton"
+          class="w-full bg-white border  shadow-[0_0_3px_0_rgba(0, 0, 0, 0.084)] px-[14px] py-[16px] shadow-[0_2px_3px_0_rgba(0, 0, 0, 0.168)] text-gray-500 rounded-[10px] focus:outline-none"
+        >
+          Continue with Google
+        </button>
+        <p className="text-neutral-100 text-[14px]">
+          tidak punya akun?{" "}
+          <Link to="/register" className="font-bold text-primaryBlue-main">
+            Buat Akun
+          </Link>
+        </p>
       </div>
+      <img
+        src={logoWhite}
+        alt="Logo"
+        className="hidden md:block absolute w-[128px] bottom-[32px] left-[32px]"
+      />
     </main>
   );
 }
