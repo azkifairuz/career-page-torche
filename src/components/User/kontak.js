82dc5bf3ef798e4c2dd17aee0e03827f5a766f74
azkifairuz <azkiajmal@gmail.com>
2023-03-23 14:23:38 +0700

    feate contact form

---
+++ src/components/User/kontak.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Textarea } from "react-bootstrap-icons";
+export default function BasicInformation(params) {
+    return(
+        <div>
+            <div>
+                <header className="border-b border-primaryBlue-main p-2">
+                    <h1 className="font-rajdhani heading-l-medium">Kontak</h1>
+                </header>
+                <form>
+                    <div className="flex flex-col">
+                        <label className="font-cairo  cairo-xl-regular">Phone</label>
+                        <input type="text" className="py-2 px-4 outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="font-cairo  cairo-xl-regular">Alamat Email</label>
+                        <input type="text" className="py-2 px-4 outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="font-cairo  cairo-xl-regular">Negara</label>
+                        <input type="text" className="py-2 px-4 outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="font-cairo  cairo-xl-regular">Kota</label>
+                        <input type="text" className="py-2 px-4 outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="font-cairo  cairo-xl-regular">Alamat</label>
+                        <textarea type="text" className="py-2 px-4 outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  ></textarea>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
