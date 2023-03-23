f45385c64c4a91b5a91e44d90388d1e401ab841c
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-03-23 14:50:57 +0800

    change folder name, add value and onchange

---
+++ src/components/atom/InputField.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function InputField(props) {
+  const { label, type, placeholder, value, onChange } = props;
+  return (
+    <div className="flex flex-col gap-[8px] w-full font-cairo">
+      <label className="font-[#151515] font-bold">{label}</label>
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className="w-full px-[16px] py-[10px] rounded-[8px] border-[1px] border-primaryNavy-border focus:outline-none focus:border-primaryN avy-focus "
+      />
+    </div>
+  );
+}
