4a5995bbd52a7feb91c763b3d6e78ab32d8545e4
azkifairuz <azkiajmal@gmail.com>
2023-03-23 14:44:40 +0700

    fix: text color contact form

--- src/components/User/Contact.js
+++ src/components/User/Contact.js
@@ -7,25 +7,25 @@ export default function Contact(params) {
                 <header className="border-b border-primaryBlue-main p-2">
                     <h1 className="font-rajdhani heading-l-medium">Kontak</h1>
                 </header>
-                <form className="flex mt-2 flex-col gap-4">
+                <form className="flex mt-2  flex-col gap-4">
                     <div className="flex flex-col">
-                        <label className="font-cairo self-start cairo-xl-regular">Phone</label>
+                        <label className="font-cairo self-start text-neutral-800 cairo-xl-regular">Phone</label>
                         <input type="text" className="py-2 px-4 outline-primaryBlue-main outline-2 rounded-md outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
                     </div>
                     <div className="flex flex-col">
-                        <label className="font-cairo  self-start cairo-xl-regular">Alamat Email</label>
+                        <label className="font-cairo  self-start text-neutral-800 cairo-xl-regular">Alamat Email</label>
                         <input type="text" className="py-2 px-4 outline-primaryBlue-main outline-2 rounded-md  outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
                     </div>
                     <div className="flex flex-col">
-                        <label className="font-cairo  self-start cairo-xl-regular">Negara</label>
+                        <label className="font-cairo  self-start text-neutral-800 cairo-xl-regular">Negara</label>
                         <input type="text" className="py-2 px-4 outline-primaryBlue-main outline-2 rounded-md outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
                     </div>
                     <div className="flex flex-col">
-                        <label className="font-cairo self-start cairo-xl-regular">Kota</label>
+                        <label className="font-cairo self-start text-neutral-800 cairo-xl-regular">Kota</label>
                         <input type="text" className="py-2 px-4 outline-primaryBlue-main outline-2 rounded-md outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
                     </div>
                     <div className="flex flex-col">
-                        <label className="font-cairo self-start cairo-xl-regular">Alamat</label>
+                        <label className="font-cairo self-start text-neutral-800 cairo-xl-regular">Alamat</label>
                         <textarea type="text" className="py-2 h-[167px] resize-none px-4 outline-primaryBlue-main outline-2 rounded-md outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  ></textarea>
                     </div>
                 </form>
