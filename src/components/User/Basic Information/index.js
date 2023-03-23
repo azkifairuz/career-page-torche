cff11e0e9303135f2ba3e074cb5674f335631df9
azkifairuz <azkiajmal@gmail.com>
2023-03-23 14:33:54 +0700

    fix:style input field basic information form

--- src/components/User/Basic Information/index.js
+++ src/components/User/Basic Information/index.js
@@ -10,31 +10,31 @@ export default function BasicInformation(params) {
                 <form>
                     <div className="flex flex-col">
                         <label className="font-cairo  cairo-xl-regular">Nama</label>
-                        <input type="text" className="py-2 px-4 outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
+                        <input type="text" className="py-2 px-4 outline-2 rounded-md outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
                     </div>
                     <div className="flex flex-col">
                         <label className="font-cairo  cairo-xl-regular">Title</label>
-                        <input type="text" className="py-2 px-4 outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
+                        <input type="text" className="py-2 px-4 outline-2 rounded-md outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
                     </div>
                     <div className="flex flex-col">
                         <label className="font-cairo  cairo-xl-regular">Bahasa</label>
-                        <input type="text" className="py-2 px-4 outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
+                        <input type="text" className="py-2 px-4 outline-2 rounded-md outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
                     </div>
                     <div className="flex flex-col">
                         <label className="font-cairo  cairo-xl-regular">Umu/Jenis Kelamin</label>
-                        <input type="text" className="py-2 px-4 outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
+                        <input type="text" className="py-2 px-4 outline-2 rounded-md outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
                     </div>
                     <div className="flex flex-col">
                         <label className="font-cairo  cairo-xl-regular">Gaj Sekarang (IDR)</label>
-                        <input type="text" className="py-2 px-4 outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
+                        <input type="text" className="py-2 px-4 outline-2 rounded-md outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
                     </div>
                     <div className="flex flex-col">
                         <label className="font-cairo  cairo-xl-regular">Gaji Yang Diharapkan(IDR)</label>
-                        <input type="text" className="py-2 px-4 outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
+                        <input type="text" className="py-2 px-4 outline-2 rounded-md outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
                     </div>
                     <div className="flex flex-col">
                         <label className="font-cairo  cairo-xl-regular">Deskripsi</label>
-                        <textarea type="text" className="py-2 px-4 outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  ></textarea>
+                        <textarea type="text" className="py-2 px-4 outline-2 rounded-md outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  ></textarea>
                     </div>
                 </form>
             </div>
