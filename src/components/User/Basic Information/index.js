63562fe912bd9b4d36d8a9800663ff22ceb4db97
azkifairuz <azkiajmal@gmail.com>
2023-03-23 14:42:20 +0700

    fix: gap

--- src/components/User/Basic Information/index.js
+++ src/components/User/Basic Information/index.js
@@ -7,33 +7,33 @@ export default function BasicInformation(params) {
                 <header className="border-b border-primaryBlue-main p-2">
                     <h1 className="font-rajdhani heading-l-medium">Basic Information</h1>
                 </header>
-                <form>
+                <form className="flex flex-col gap-4 mt-2">
                     <div className="flex flex-col">
-                        <label className="font-cairo  cairo-xl-regular">Nama</label>
+                        <label className="font-cairo self-start  cairo-xl-regular">Nama</label>
                         <input type="text" className="py-2 px-4 outline-2 rounded-md outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
                     </div>
                     <div className="flex flex-col">
-                        <label className="font-cairo  cairo-xl-regular">Title</label>
+                        <label className="font-cairo self-start cairo-xl-regular">Title</label>
                         <input type="text" className="py-2 px-4 outline-2 rounded-md outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
                     </div>
                     <div className="flex flex-col">
-                        <label className="font-cairo  cairo-xl-regular">Bahasa</label>
+                        <label className="font-cairo self-start cairo-xl-regular">Bahasa</label>
                         <input type="text" className="py-2 px-4 outline-2 rounded-md outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
                     </div>
                     <div className="flex flex-col">
-                        <label className="font-cairo  cairo-xl-regular">Umu/Jenis Kelamin</label>
+                        <label className="font-cairo self-start cairo-xl-regular">Umu/Jenis Kelamin</label>
                         <input type="text" className="py-2 px-4 outline-2 rounded-md outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
                     </div>
                     <div className="flex flex-col">
-                        <label className="font-cairo  cairo-xl-regular">Gaj Sekarang (IDR)</label>
+                        <label className="font-cairo self-start cairo-xl-regular">Gaj Sekarang (IDR)</label>
                         <input type="text" className="py-2 px-4 outline-2 rounded-md outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
                     </div>
                     <div className="flex flex-col">
-                        <label className="font-cairo  cairo-xl-regular">Gaji Yang Diharapkan(IDR)</label>
+                        <label className="font-cairo self-start cairo-xl-regular">Gaji Yang Diharapkan(IDR)</label>
                         <input type="text" className="py-2 px-4 outline-2 rounded-md outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
                     </div>
                     <div className="flex flex-col">
-                        <label className="font-cairo  cairo-xl-regular">Deskripsi</label>
+                        <label className="font-cairo self-start cairo-xl-regular">Deskripsi</label>
                         <textarea type="text" className="py-2 px-4 h-[167px] resize-none outline-2 rounded-md outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  ></textarea>
                     </div>
                 </form>
