c9bd43a8615025b60b12aaa8f296779f49460d59
azkifairuz <azkiajmal@gmail.com>
2023-03-23 17:20:07 +0700

    fix: navbar user responsive

--- src/components/User/navbar.js
+++ src/components/User/navbar.js
@@ -19,45 +19,47 @@ export default function Navbar() {
   return (
     <>
       <nav className="sticky z-50 top-0 w-full font-rajdhani px-7 py-1 font-medium bg-primaryNavy-main text-white flex items-center justify-between">
-        <div>
-          <Link to="/" className="cursor-pointer">
-            <img className="w-[128px] h-[72px]" src={Logo} alt="Logo" />
-          </Link>
-        </div>
-        <div>
-          <button
-            onClick={handleNavbarOpen}
-            className={`${isNavbarOpen && "hidden"} lg:hidden hamburger`}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
+      <div className=" md:gap-2 md:justify-start justify-between w-full flex items-center">
+          <div>
+            <Link to="/" className="cursor-pointer">
+              <img className="w-[128px] h-[72px]" src={Logo} alt="Logo" />
+            </Link>
+          </div>
+          <div>
+            <button
+              onClick={handleNavbarOpen}
+              className={`${isNavbarOpen && "hidden"} md:hidden hamburger`}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-              />
-            </svg>
-          </button>
-          <ul className="lg:flex hidden items-center mt-0">
-            {navbarData.map((data, index) => {
-              return (
-                <li
-                  key={index}
-                  className="lg:mx-3 lg:my-0 my-3 hover:text-[#0362cb] transition duration-200 "
-                >
-                  <Link to={data.href}>{data.title}</Link>
-                </li>
-              );
-            })}
-          </ul>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            </button>
+            <ul className="md:flex hidden items-center mt-0">
+              {navbarData.map((data, index) => {
+                return (
+                  <li
+                    key={index}
+                    className="md:mx-3 md:my-0 my-3 hover:text-[#0362cb] transition duration-200 "
+                  >
+                    <Link to={data.href}>{data.title}</Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
-        <div className="lg:flex hidden justify-center">
+        <div className="md:flex hidden justify-center">
           <Link
             to="/landing"
             className="bg-[#5885E9] rounded-full py-2 px-[20px] text-[16px] font-cairo font-bold"
@@ -69,14 +71,14 @@ export default function Navbar() {
       <div
         className={`${
           isNavbarOpen ? "block" : "hidden"
-        } backdrop lg:hidden absolute z-40 left-0 top-0 right-0 min-h-screen`}
+        } backdrop md:hidden absolute z-40 left-0 top-0 right-0 min-h-screen`}
         style={{ background: "rgba(23, 35, 46, 0.9)" }}
       ></div>
       <button
         onClick={handleNavbarOpen}
         className={`${
           isNavbarOpen ? "block" : "hidden"
-        } lg:hidden absolute right-5 top-5 z-50 text-white`}
+        } md:hidden absolute right-5 top-5 z-50 text-white`}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -96,13 +98,13 @@ export default function Navbar() {
       <ul
         className={`${
           isNavbarOpen ? "block" : "hidden"
-        } lg:hidden absolute w-full text-white bg-primaryNavy-main flex flex-col mt-0 bg-red p-3 z-50 min-h-[calc(100%-70px)]`}
+        } md:hidden absolute w-full text-white bg-primaryNavy-main flex flex-col mt-0 bg-red p-3 z-50 min-h-[calc(100%-70px)]`}
       >
         {navbarData.map((data, index) => {
           return (
             <li
               key={index}
-              className="relative lg:mx-3 lg:my-0 my-3 transition duration-200 font-light"
+              className="relative md:mx-3 md:my-0 my-3 transition duration-200 font-light"
             >
               <div>{data.title}</div>
             </li>
