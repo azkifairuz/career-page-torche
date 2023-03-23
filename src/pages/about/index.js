c076b401c133e13a794acabcc27b5f1f6b4afa8f
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-03-24 05:54:55 +0800

    fix:remove repeating apply cairo

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,8 +1,15 @@
 function About() {
   return (
-    <div className="text-center">
-      <h1>About</h1>
-    </div>
+    <main className="flex flex-col items-center">
+      <section className="flex gap-[200px] items-center">
+        <h1 className="heading-l-bold">
+          About Torche <br /> Education
+        </h1>
+        <p>
+          
+        </p>
+      </section>
+    </main>
   );
 }
 export default About;
