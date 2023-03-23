bf8be07e7305149b7754a942731ec0cf7d7df74b
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-03-23 18:33:07 +0800

    refactor navbar

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-import Logo from "../../assets/logos/Torche_Logo-01_White.png";
+import Logo from "../../assets/logos/Torche_Logo-01_White.webp";
 
 export default function Navbar() {
   const [isNavbarOpen, setIsNavbarOpen] = useState(false);
@@ -13,98 +13,98 @@ export default function Navbar() {
   const navbarData = [
     { href: "/", title: "Home" },
     { href: "/joblist", title: "Job List" },
-    { href: "/about", title: "About" },
+    { href: "/about", title: "About Torche" },
+    { href: "/faq", title: "FAQ" },
   ];
 
   return (
-    <>
-      <nav className="sticky z-50 top-0 w-full font-rajdhani px-7 py-1 font-medium bg-primaryNavy-main text-white flex items-center justify-between">
-        <div>
+    <nav className="sticky z-50 top-0 w-screen bg-primaryNavy-main ">
+      <div className=" font-rajdhani py-[1px] px-[16px] lg:px-[100px] font-medium  text-white flex items-center justify-between h-fit">
+        <section className="flex gap-[24px]">
           <Link to="/" className="cursor-pointer">
             <img className="w-[128px] h-[72px]" src={Logo} alt="Logo" />
           </Link>
-        </div>
-        <div>
-          <button
-            onClick={handleNavbarOpen}
-            className={`${isNavbarOpen && "hidden"} lg:hidden hamburger`}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-              />
-            </svg>
-          </button>
-          <ul className="lg:flex hidden items-center mt-0">
+          <ul className="hidden lg:flex items-center">
             {navbarData.map((data, index) => {
               return (
                 <li
                   key={index}
-                  className="lg:mx-3 lg:my-0 my-3 hover:text-[#0362cb] transition duration-200 "
+                  className="px-[16px] py-[8px] hover:text-[#0362cb] transition duration-200 "
                 >
                   <Link to={data.href}>{data.title}</Link>
                 </li>
               );
             })}
           </ul>
-        </div>
-        <div className="lg:flex hidden justify-center">
+        </section>
+        <button
+          onClick={handleNavbarOpen}
+          className={`${isNavbarOpen && "hidden"} lg:hidden hamburger`}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </button>
+        <button
+          onClick={handleNavbarOpen}
+          className={`${
+            isNavbarOpen ? "block" : "hidden"
+          } lg:hidden absolute right-5 top-5 z-50 text-white`}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+        <section className="hidden lg:flex gap-[4px] justify-center">
+          <Link
+            to="/signup"
+            className="bg-primaryBlue-surface text-neutral-1000 text-[16px] font-rajdhani font-semibold rounded-[8px] p-[8px] px-[16px]"
+          >
+            Register
+          </Link>
           <Link
             to="/login"
-            className="bg-[#5885E9] rounded-full py-2 px-[20px] text-[16px] font-cairo font-bold"
+            className="text-neutral-100 text-[16px] font-rajdhani font-semibold rounded-[8px] p-[8px] px-[16px]"
           >
             Login
           </Link>
-        </div>
-      </nav>
-      <div
-        className={`${
-          isNavbarOpen ? "block" : "hidden"
-        } backdrop lg:hidden absolute z-40 left-0 top-0 right-0 min-h-screen`}
-        style={{ background: "rgba(23, 35, 46, 0.9)" }}
-      ></div>
-      <button
-        onClick={handleNavbarOpen}
-        className={`${
-          isNavbarOpen ? "block" : "hidden"
-        } lg:hidden absolute right-5 top-5 z-50 text-white`}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
-      </button>
+        </section>
+      </div>
       <ul
         className={`${
           isNavbarOpen ? "block" : "hidden"
-        } lg:hidden absolute w-full text-white bg-primaryNavy-main flex flex-col mt-0 bg-red p-3 z-50 min-h-[calc(100%-70px)]`}
+        } lg:hidden w-full text-white bg-primaryNavy-main flex flex-col mt-0 bg-red p-3 z-50 `}
       >
         {navbarData.map((data, index) => {
           return (
             <li
               key={index}
-              className="relative lg:mx-3 lg:my-0 my-3 transition duration-200 font-light"
+              className="relative p-[12px] px-[16px] transition duration-200 font-light"
+              onClick={handleNavbarOpen}
             >
-              <div>{data.title}</div>
+              <Link to={data.href}>{data.title}</Link>
             </li>
           );
         })}
@@ -117,6 +117,6 @@ export default function Navbar() {
           </a>
         </div>
       </ul>
-    </>
+    </nav>
   );
 }
