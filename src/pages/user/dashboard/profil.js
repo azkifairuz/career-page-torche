c5b1b3c835ba7c68fd653014485973c7307ada53
azkifairuz <azkiajmal@gmail.com>
2023-03-23 17:56:59 +0700

    feat:add user page

---
+++ src/pages/user/dashboard/profil.js
@@ -0,0 +1,11 @@
+import BasicInformation from "components/User/Basic Information";
+import Contact from "components/User/Contact";
+
+export default function profil(params) {
+    return(
+        <>
+        <BasicInformation/>
+        <Contact/>
+        </>
+    )
+}
