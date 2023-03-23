14bc10f038e5a37b59ac73a01d3b0f9eb5253388
azkifairuz <azkiajmal@gmail.com>
2023-03-23 14:48:07 +0700

    fix: gap between label and text field

--- src/components/User/Basic Information/index.js
+++ src/components/User/Basic Information/index.js
@@ -7,31 +7,31 @@ export default function BasicInformation(params) {
                     <h1 className="font-rajdhani heading-l-medium">Basic Information</h1>
                 </header>
                 <form className="flex flex-col gap-4 mt-2">
-                    <div className="flex flex-col">
+                    <div className="flex flex-col gap-1">
                         <label className="font-cairo self-start text-neutral-800  cairo-xl-regular">Nama</label>
                         <input type="text" className="py-2 px-4 outline-2 rounded-md outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
                     </div>
-                    <div className="flex flex-col">
+                    <div className="flex flex-col gap-1">
                         <label className="font-cairo self-start text-neutral-800 cairo-xl-regular">Title</label>
                         <input type="text" className="py-2 px-4 outline-2 rounded-md outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
                     </div>
-                    <div className="flex flex-col">
+                    <div className="flex flex-col gap-1">
                         <label className="font-cairo self-start text-neutral-800 cairo-xl-regular">Bahasa</label>
                         <input type="text" className="py-2 px-4 outline-2 rounded-md outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
                     </div>
-                    <div className="flex flex-col">
+                    <div className="flex flex-col gap-1">
                         <label className="font-cairo self-start text-neutral-800 cairo-xl-regular">Umu/Jenis Kelamin</label>
                         <input type="text" className="py-2 px-4 outline-2 rounded-md outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
                     </div>
-                    <div className="flex flex-col">
+                    <div className="flex flex-col gap-1">
                         <label className="font-cairo self-start text-neutral-800 cairo-xl-regular">Gaj Sekarang (IDR)</label>
                         <input type="text" className="py-2 px-4 outline-2 rounded-md outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
                     </div>
-                    <div className="flex flex-col">
+                    <div className="flex flex-col gap-1">
                         <label className="font-cairo self-start text-neutral-800 cairo-xl-regular">Gaji Yang Diharapkan(IDR)</label>
                         <input type="text" className="py-2 px-4 outline-2 rounded-md outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  />
                     </div>
-                    <div className="flex flex-col">
+                    <div className="flex flex-col gap-1">
                         <label className="font-cairo self-start text-neutral-800 cairo-xl-regular">Deskripsi</label>
                         <textarea type="text" className="py-2 px-4 h-[167px] resize-none outline-2 rounded-md outline-primaryBlue-main outline focus:outline-primaryBlue-main focus:ring-1 focus:ring-primaryBlue-main"  ></textarea>
                     </div>
