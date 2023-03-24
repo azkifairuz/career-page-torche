1cbf09c7f977801c389ea2edc36b8e258a27f8f3
azkifairuz <azkiajmal@gmail.com>
2023-03-24 22:10:26 +0700

    fix:preview pengalaman

--- src/components/User/Complete Profile/preview/index.js
+++ src/components/User/Complete Profile/preview/index.js
@@ -1,9 +1,11 @@
+import Pengalaman from "./experience";
 import Tentang from "./tentang";
 
 export default function Preview(params) {
     return(
         <>
             <Tentang/>
+            <Pengalaman/>
         </>
     )
 }
