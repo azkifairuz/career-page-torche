189cd92288a75ceae092b6a7ccff2849283afee5
azkifairuz <azkiajmal@gmail.com>
2023-03-24 17:54:59 +0700

    feat:add item progress bar

--- src/components/User/Complete Profile/header/progress/card.js
+++ src/components/User/Complete Profile/header/progress/card.js
@@ -1,5 +1,13 @@
+import Item from "./item";
+
 export default function ProgressCard(props) {
   const { percent } = props;
+  const itemData = [
+    { href: "/tentang", title: "Tentang" },
+    { href: "/pengelaman", title: "Pengalaman Kerja" },
+    { href: "/pendidikan", title: "Pendidikan" },
+    { href: "/organisasi", title: "Organisasi" },
+  ];
   return (
     <div>
       <div className="flex gap-2">
@@ -11,6 +19,11 @@ export default function ProgressCard(props) {
           <div className="absolute w-1/2  bg-primaryBlue-main top-1/2 left-0 h-1  -translate-y-1/2 -z-10 rounded-md"></div>
           <div className="absolute  bg-neutral-300 top-1/2 left-0 h-1 -translate-y-1/2 -z-20 rounded-md w-full  "></div>
         </div>
+      </div>
+      <div>
+        {itemData.map((item)=>{
+          return <Item href={item.href} title={item.title} />
+        })}
       </div>
     </div>
   );
