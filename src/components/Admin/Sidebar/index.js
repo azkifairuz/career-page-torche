33362d77307e70c7ba9f76bb7644344c41d81aaa
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-03-27 10:20:42 +0800

    refactor sidebar

--- src/components/Admin/Sidebar/index.js
+++ src/components/Admin/Sidebar/index.js
@@ -1,10 +1,13 @@
-import * as Icon from "react-bootstrap-icons";
-
-import Logo from "assets/icons/TorcheDarkBg.svg";
-import Item from "./Item";
-
 import { useAuthDispatch, logout } from "context";
 import { Link, Navigate } from "react-router-dom";
+
+import Item from "./Item";
+
+import Logo from "assets/icons/TorcheDarkBg.svg";
+import DashboardIcon from "assets/icons/DashboradIcon.svg";
+import LowonganIcon from "assets/icons/LowonganIcon.svg";
+import PelamarIcon from "assets/icons/PelamarIcon.svg";
+import WawancaraIcon from "assets/icons/WawancaraIcon.svg";
 
 export default function Sidebar(props) {
   const dispatch = useAuthDispatch();
@@ -15,44 +18,21 @@ export default function Sidebar(props) {
   };
 
   return (
-    <aside className="box-border flex flex-col text-black border-r-2 sticky top-0 left-0 h-screen pt-[152px] px-[80px]">
+    <aside className="box-border flex flex-col text-black border-r-2 sticky top-0 left-0 h-screen pt-[100px] px-[60px]">
       <main className="flex flex-col">
         <div className="flex gap-[12px] p-[12px] items-center bg-neutral-200 rounded-[12px]">
           <img src={Logo} alt="Torche Logo" className="w-[44px]" />
           <section className="flex flex-col gap-[4px]">
-            <h2 className="heading-s-bold">
-              Torche Education
-            </h2>
-            <p className="heading-s-medium">
-              Admin
-            </p>
+            <h2 className="heading-s-bold">Torche Education</h2>
+            <p className="heading-s-medium">Admin</p>
           </section>
         </div>
         <ul className="flex flex-col gap-2 mt-14">
-          <Item
-            icon={<Icon.Grid size={20} />}
-            title="Dashboard"
-            href="dashboard"
-          />
-          <Item
-            icon={<Icon.BriefcaseFill size={20} />}
-            title="Lowongan"
-            href="vacancy"
-          />
-          <Item
-            icon={<Icon.PeopleFill size={20} />}
-            title="Pelamar"
-            href="applicants"
-          />
-          <Item
-            icon={<Icon.GearFill size={20} />}
-            title="Pengaturan"
-            href="settings"
-          />
+          <Item src={DashboardIcon} title="Dashboard" href="dashboard" />
+          <Item src={LowonganIcon} title="Lowongan" href="vacancy" />
+          <Item src={PelamarIcon} title="Pelamar" href="applicants" />
+          <Item src={WawancaraIcon} title="Pengaturan" href="settings" />
         </ul>
-        <button onClick={handleLogout}>
-          <Link to="/login">Keluar</Link>
-        </button>
       </main>
     </aside>
   );
