ffd35fe357aa13aa2d7abf6e0aa5dcec5e7a9237
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-03-27 09:50:55 +0800

    fix:dynamic width

--- src/pages/admin/dashboard/Card.js
+++ src/pages/admin/dashboard/Card.js
@@ -1,7 +1,7 @@
 export default function Card(props) {
   const { count, title, icon } = props
   return (
-    <div className="flex justify-between p-[12px] w-[294px] shadow-[0_0_8px_rgba(0,0,0,0.25)] rounded-[12px] flex-grow">
+    <div className="flex justify-between p-[12px] flex-1 shadow-[0_0_8px_rgba(0,0,0,0.25)] rounded-[12px] flex-grow">
       <section className="flex flex-col gap-[4px]">
         <h1 className="heading-l-bold">{count}</h1>
         <h3 className="text-xl-regular w-[129px]">{title}</h3>
