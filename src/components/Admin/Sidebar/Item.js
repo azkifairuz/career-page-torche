1fbd903ef208f3d734b5f5ff26348ea75e2e33eb
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-03-27 10:20:55 +0800

    change icon and spacing

--- src/components/Admin/Sidebar/Item.js
+++ src/components/Admin/Sidebar/Item.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Item(props) {
-  const { icon, title, href } = props;
+  const { src, title, href } = props;
   return (
     <Link
       to={href}
-      className="flex gap-6 items-center px-4 py-3 hover:bg-slate-500 rounded-lg font-rajdhani font-medium"
+      className="flex gap-[20px] items-center px-4 py-3 hover:bg-neutral-300 rounded-lg"
     >
-      {icon}
-      <span>{title}</span>
+      <img src={src} alt={title} className="w-[22px]"/>
+      <p className="heading-s-medium">{title}</p>
     </Link>
   );
 }
