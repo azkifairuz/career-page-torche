31153018607054b493b928ef9178f3a4b92f5334
azkifairuz <azkiajmal@gmail.com>
2023-03-28 00:46:01 +0700

    fix:pages

--- src/components/User/Complete Profile/index.js
+++ src/components/User/Complete Profile/index.js
@@ -1,19 +1,1 @@
-import HeaderSection from "./header";
-import OptionBar from "./option bar";
-import Preview from "./preview";
 
-export default function CompleteProfil() {
-  return (
-    <div className="flex flex-col">
-      {<HeaderSection />}
-      <div className="lg:flex w-full  lg:px-[100px]">
-        <div className="mt-6 p-6">
-          <OptionBar />
-        </div>
-        <div className="lg:w-[819px]">
-          <Preview />
-        </div>
-      </div>
-    </div>
-  );
-}
--- src/pages/user/complete profile/CompleteProfile.js
+++ src/pages/user/complete profile/CompleteProfile.js
@@ -1,90 +1,20 @@
-import { useEffect, useState } from "react";
-
-import Custom404 from "components/Custom404";
-import * as Icon from "react-bootstrap-icons";
-// import ProgressBar from "./ProgressBar";
-import Circle from "components/User/Complete Profile/Circle";
 import HeaderSection from "components/User/Complete Profile/header";
-import CompleteProfil from "components/User/Complete Profile";
-function Completeprofil() {
+import OptionBar from "components/User/Complete Profile/option bar";
+import Preview from "components/User/Complete Profile/preview";
 
-    return(
-      <div>
-        <CompleteProfil/>
+
+export default function CompleteProfil() {
+  return (
+    <div className="flex flex-col">
+      {<HeaderSection />}
+      <div className="lg:flex w-full  lg:px-[100px]">
+        <div className="mt-6 p-6">
+          <OptionBar />
+        </div>
+        <div className="lg:w-[819px]">
+          <Preview />
+        </div>
       </div>
-    )
-  // const [page, setPage] = useState(1);
-  // const [active, setActive] = useState(0);
-  // const [circle] = useState(3);
-  // const [width, setWidth] = useState(0);
-
-  // useEffect(() => {
-  //   setWidth((100 / (circle - 1)) * active);
-  // }, [circle, active]);
-
-  // function nextPage() {
-  //   return active >= circle
-  //     ? setActive(circle)
-  //     : setActive(active + 1) & setPage(page + 1);
-  // }
-  // function backPage() {
-  //   return active <= 0
-  //     ? setActive(0)
-  //     : setActive(active - 1) & setPage(page - 1);
-  // }
-  // console.log(active);
-
-  // function ProgressBar() {
-  //   const progress = [];
-  //   for (let i = 0; i < circle; i++) {
-  //     progress.push(
-  //       <Circle
-  //         classname={
-  //           i <= active
-  //             ? "border-blue-500 border-[3px] bg-white  text-md bg-whitetext-[#999]  rounded-full  h-8 w-8 flex items-center justify-center "
-  //             : "border-[3px]  text-md bg-white border-[#e0e0e0] text-[#999]  rounded-full ease-linear delay-[0.4s] h-8 w-8 flex items-center justify-center "
-  //         }
-  //         done={active <= i ? " " : <Icon.Check />}
-  //       />
-  //     );
-
-  //   }
-  //   return (
-  //     <div className="box-border mb-20 -mt-10 flex justify-center items-center w-[100vw]">
-  //       <div className="items-center w-1/2 ">
-  //         <div className="flex justify-between items-center relative">
-  //           <div
-  //             className="absolute  bg-blue-500 top-1/2 left-0 h-1  -translate-y-1/2 -z-10 rounded-md"
-
-  //           ></div>
-  //           <div className="absolute  bg-slate-200 top-1/2 left-0 h-1 -translate-y-1/2 -z-20 rounded-md w-full  "></div>
-  //           {progress}
-  //         </div>
-  //       </div>
-  //     </div>
-  //   );
-  // }
-
-  // function viewPager() {
-  //   if (page === 1) {
-  //     return <Biodata next={nextPage} />;
-  //   }
-  //   if (page === 2) {
-  //     return <Education back={backPage} next={nextPage} />;
-  //   }
-  //   if (page >= 3) {
-  //     return <Experience back={backPage} />;
-  //   }
-  //   return <Custom404 />;
-  // }
-
-  // return (
-  //   <>
-  //     <h1 className="text-2xl md:text-4xl font-bold text-center mt-5 -mb-10" >Complete Profil</h1>
-  //     {viewPager()}
-  //     {ProgressBar()}
-  //   </>
-  // );
+    </div>
+  );
 }
-
-export default Completeprofil;
