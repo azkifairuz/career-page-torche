d40864972886717dfc25bf825389b30e3f0a8106
azkifairuz <azkiajmal@gmail.com>
2023-03-28 20:18:28 +0700

    fix:category card

--- src/data/category.js
+++ src/data/category.js
@@ -1,42 +1,49 @@
-import * as Icon  from "react-bootstrap-icons"
+import finance from "assets/icons/dolar.svg"
+import generalAffairs from "assets/icons/general-affairs.svg"
+import techeng from "assets/icons/techeng.svg"
+import sales from "assets/icons/sales.svg"
+import hr from "assets/icons/hr.svg"
+import curiculum from "assets/icons/curiculum.svg"
+import tutors from "assets/icons/tutors.svg"
+import ceo from "assets/icons/ceo.svg"
 export const categories = [
     {
-        icon: <Icon.Cash size={50} color="blue"/>,
+        icon: finance,
         Category: "Finance",
         Jumlah:0
     },
     {
-        icon: <Icon.Person size={50} color="blue"/>,
+        icon: generalAffairs,
         Category: "General Affairs",
         Jumlah:2
     },
     {
-        icon: <Icon.Person size={50} color="blue"/>,
+        icon: curiculum ,
         Category: "Curiculum And Education",
         Jumlah:0
     },
     {
-        icon: <Icon.Person size={50} color="blue"/>,
+        icon: ceo,
         Category: "CEO Office",
         Jumlah:0
     },
     {
-        icon: <Icon.Person size={50} color="blue"/>,
+        icon:hr,
         Category: "Human Resource",
         Jumlah:0
     },
     {
-        icon: <Icon.Person size={50} color="blue"/>,
+        icon: sales,
         Category: "Marketin And Sales ",
         Jumlah:0
     },
     {
-        icon: <Icon.Person size={50} color="blue"/>,
+        icon: techeng,
         Category: "Technologi And Engineering",
         Jumlah:0
     },
     {
-        icon: <Icon.Person size={50} color="blue"/>,
+        icon: tutors,
         Category: "Tutors",
         Jumlah:0
     },
