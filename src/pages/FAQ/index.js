b1fb5117e4d6a2a9c0cf999e150f277cdaf9240d
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-03-28 15:03:33 +0800

    complete faq page

--- src/pages/FAQ/index.js
+++ src/pages/FAQ/index.js
@@ -6,37 +6,47 @@ import Interview from "assets/icons/interview.svg";
 import Offering from "assets/icons/offering.svg";
 
 export default function FAQ() {
+  const cardData = [
+    {
+      title: "Screening CV",
+      src: ScreeningCv,
+    },
+    {
+      title: "Screening Test",
+      src: ScreeningTest,
+    },
+    {
+      title: "Interview",
+      src: Interview,
+    },
+    {
+      title: "Offering",
+      src: Offering,
+    },
+  ];
+
   return (
     <section className="flex-1">
       <header className="bg-primaryNavy-hover text-white flex items-center justify-center p-[120px]">
         <h1 className="display-bold">FAQ</h1>
       </header>
-      <main className="px-[16px] md:px[32px] lg:px-[100px] py-[80px]">
+      <main className="flex flex-col px-[16px] md:px[32px] lg:px-[100px] py-[80px] gap-[80px]">
         <div className="flex flex-col gap-[40px]">
           <h1 className="heading-l-bold w-fit md:self-center">
             Hiring Process
           </h1>
           <section className="flex flex-wrap lg:flex-nowrap gap-[24px] ">
-            <Card
-              title="Screening CV"
-              src={ScreeningCv}
-              step="1"
-            />
-            <Card
-              title="Screening Test"
-              src="assets/icons/ScreeningTest.svg"
-              step="2"
-            />
-            <Card title="Interview" src="assets/icons/Interview.svg" step="3" />
-            <Card title="Offering" src="assets/icons/Offering.svg" step="4" />
-          </section>
-          <section className="flex flex-col gap-[40px]">
-            <h1 className="heading-l-bold w-fit md:self-center">
-              Frequently Asked Questions
-            </h1>
-            <Accordion />
+            {cardData.map((card, index) => (
+              <Card key={index} {...card} step={index + 1} />
+            ))}
           </section>
         </div>
+        <section className="flex flex-col gap-[40px]">
+          <h1 className="heading-l-bold w-fit md:self-center">
+            Frequently Asked Questions
+          </h1>
+          <Accordion />
+        </section>
       </main>
     </section>
   );
