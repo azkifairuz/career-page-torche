371d1849d6b1bd6211e9f96b5cf22be265662d0d
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-03-28 14:55:10 +0800

    feat:dynamic panel

---
+++ src/pages/FAQ/Accordion/Panel.js
@@ -0,0 +1,37 @@
+import ArrowDown from "assets/icons/arrow-circle-down.svg";
+
+export default function Panel(props) {
+  const { title, content, setActiveIndex, isActive, index } = props;
+
+  const handleClick = () => {
+    if (!isActive) {
+      setActiveIndex(index);
+    } else {
+      setActiveIndex(null);
+    }
+  };
+
+  return (
+    <div className="flex flex-col bg-neutral-100 gap-[44px] rounded-[16px] py-[20px] px-[24px] shadow-[0_1px_16px_rgba(0,0,0,0.12)] transition-all duration-1000 linear">
+      <seciton className="flex justify-between">
+        <h1 className="text-xl-regular text-neutral-1000">{title}</h1>
+        <button className="shrink-0" onClick={handleClick}>
+          <img
+            src={ArrowDown}
+            className={`${
+              isActive && "rotate-180"
+            } transition-all duration-300 ease-in-out`}
+            alt="Arrow"
+          />
+        </button>
+      </seciton>
+      <p
+        className={`${
+          !isActive && "hidden"
+        } text-xl-regular text-neutral-1000 transition-all duration-700 ease-in-out`}
+      >
+        {content}
+      </p>
+    </div>
+  );
+}
