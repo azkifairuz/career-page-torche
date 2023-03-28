18f3f781695f451a6b41c72c11cbfa8c27a0df5c
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-03-28 14:52:57 +0800

    feat:responsive design

--- src/pages/FAQ/Card.js
+++ src/pages/FAQ/Card.js
@@ -1,7 +1,7 @@
 export default function Card(props) {
   const {title, src, step} = props;
   return (
-    <div className='flex-1 px-[32px] py-[24px] flex flex-col gap-[16px] bg-neutral-100 shadow-[0_1px_24px_rgba(0,0,0,0.12)] rounded-[16px]'>
+    <div className='w-full md:w-[48%] grow px-[32px] py-[24px] flex flex-col gap-[16px] bg-neutral-100 shadow-[0_1px_24px_rgba(0,0,0,0.12)] rounded-[16px]'>
       <img src={src} alt={title} />
       <section>
         <h2 className="text-neutral-1000 heading-s-bold">{title}</h2>
