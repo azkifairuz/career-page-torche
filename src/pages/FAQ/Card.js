188b73d4a3aebf7bd17da9288bffc7e020f580bd
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-03-28 15:03:42 +0800

    fix:icon width

--- src/pages/FAQ/Card.js
+++ src/pages/FAQ/Card.js
@@ -2,7 +2,7 @@ export default function Card(props) {
   const {title, src, step} = props;
   return (
     <div className='w-full md:w-[48%] grow px-[32px] py-[24px] flex flex-col gap-[16px] bg-neutral-100 shadow-[0_1px_24px_rgba(0,0,0,0.12)] rounded-[16px]'>
-      <img src={src} alt={title} />
+      <img src={src} alt={title} className="w-[64px]" />
       <section>
         <h2 className="text-neutral-1000 heading-s-bold">{title}</h2>
         <p className="text-primaryBlue-main text-m-bold">Step {step}</p>
