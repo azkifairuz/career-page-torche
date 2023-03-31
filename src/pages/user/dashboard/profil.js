cd9ce1faa648e14310f1c214fc9cebd748af0245
azkifairuz <azkiajmal@gmail.com>
2023-03-31 23:22:41 +0700

    fix:delete unused file

--- src/pages/user/dashboard/profil.js
+++ src/pages/user/dashboard/profil.js
@@ -1,5 +1,4 @@
-import BasicInformation from "components/User/Basic Information";
-import Contact from "components/User/Contact";
+
 import Form from "components/User/form";
 
 export default function Profil(params) {
