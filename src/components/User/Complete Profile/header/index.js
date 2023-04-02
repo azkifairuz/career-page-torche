61801aa4df7fe67d6fe2d0e0d8cb35e519b38fda
azkifairuz <azkiajmal@gmail.com>
2023-04-03 00:00:33 +0700

    header section

--- src/components/User/Complete Profile/header/index.js
+++ src/components/User/Complete Profile/header/index.js
@@ -12,8 +12,8 @@ export default function HeaderSection() {
     { Title: "Nationality", Desc: "indonesia" },
   ];
   return (
-    <div className={`px-6 pt-2 lg:h-[400px] lg:justify-between lg:px-24 lg:py-10 lg:flex lg:bg-[url('assets/images/hero.png')] lg:text-white`}>
-      <div className="flex flex-col lg:flex-row lg:gap-6">
+    <div className={`px-6 pt-2 lg:h-[400px] md:h-[520px] md:p-8  lg:justify-between lg:px-24 lg:py-10 lg:flex md:bg-[url('assets/images/hero.png')] md:text-white`}>
+      <div className="flex flex-col md:flex-row md:gap-6">
       <div className="">
         {
           <Header
@@ -23,12 +23,12 @@ export default function HeaderSection() {
         }
          
       </div>
-      <div className="lg:w-[417px]">
+      <div className="lg:w-[417px] md:w-full">
         <div>
-            <h1 className="font-rajdhani text-center  text-black capitalize heading-l-bold lg:text-white lg:text-start">John Kebab</h1>
-            <p className="font-rajdhani heading-s-medium  text-black  text-center capitalize lg:text-white lg:text-start">Web Developer</p>
+            <h1 className="font-rajdhani text-center  text-black capitalize heading-l-bold md:text-white md:text-start">John Kebab</h1>
+            <p className="font-rajdhani heading-s-medium  text-black  text-center capitalize md:text-white md:text-start">Web Developer</p>
         </div>
-          <div className="grid grid-cols-2 items-center gap-2 lg:items-start ">
+          <div className="grid grid-cols-2 items-center gap-2  md:grid-cols-3   md:items-start ">
           {information.map((info) => {
             return <Item title={info.Title} desc={info.Desc} />;
           })}
