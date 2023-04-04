aca6e02b2284813b3db84877e9193ed0233c6fdc
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-04-04 20:55:03 +0800

    fix:unit size, responsiveness, pagination

--- src/pages/joblist/Card.js
+++ src/pages/joblist/Card.js
@@ -3,32 +3,32 @@ import { Link } from "react-router-dom";
 import TimerIcon from "assets/icons/timer.svg";
 
 export default function Card(props) {
-  const { title, place, type, category, src, id } = props;
+  const { name, place, type, category, src, id } = props;
   return (
     <Link
-      className="rounded-[8px] bg-neutral-100 shadow-[0_1px_16px_rgba(0,0,0,0.2)] cursor-pointer hover:bg-neutral-1000 hover:bg-opacity-5"
+      className="rounded-lg bg-neutral-100 shadow-[0_1px_16px_rgba(0,0,0,0.2)] cursor-pointer hover:bg-neutral-1000 hover:bg-opacity-5"
       to={id}
     >
       <header>
         <img
           src={src}
-          alt={title}
-          className="w-full h-[200px] rounded-t-[8px] object-cover"
+          alt={name}
+          className="w-full h-[140px] md:h-[200px] rounded-t-2 object-cover object-left-bottom"
         />
       </header>
-      <main className="flex flex-col gap-[8px] p-[16px]">
-        <h1 className="heading-s-medium md:heading-m-medium">{title}</h1>
-        <section className="text-neutral-1000 flex gap-[4px] text-s-regular md:text-m-regular">
-          <span className="px-[8px] py-[4px] bg-secondary-yellow rounded-[8px]">
+      <main className="flex flex-col gap-2 p-4">
+        <h1 className="heading-s-medium md:heading-m-medium">{name}</h1>
+        <section className="text-neutral-1000 flex gap-1 text-s-regular md:text-m-regular">
+          <span className="px-2 py-1 bg-secondary-yellow rounded-lg ">
             {type}
           </span>
-          <span className="px-[8px] py-[4px] bg-secondary-tosca rounded-[8px]">
+          <span className="px-2 py-1 bg-secondary-tosca rounded-lg">
             {category}
           </span>
         </section>
         <section className="text-neutral-1000 flex justify-between items-center text-m-regular md:text-l-regular">
           <p>{place}</p>
-          <span className="flex items-center gap-[4px]">
+          <span className="flex items-center gap-1">
             <img src={TimerIcon} alt="timer" />
             <p>3d left</p>
           </span>
--- src/pages/joblist/index.js
+++ src/pages/joblist/index.js
@@ -9,30 +9,43 @@ import Background from "assets/images/bg.webp";
 export default function Joblist() {
   return (
     <section className="flex-1">
-      <header className="box-border flex items-center justify-center p-[120px]">
+      <header className="box-border flex items-center justify-center py-14 md:p-[120px]">
         <img
           src={Background}
           alt="background"
-          className="absolute w-full top-0 h-[85vh] -z-10"
+          className="hidden md:block absolute w-full top-0 h-[580px] -z-10"
         />
-        <h1 className="heading-xxl-bold text-neutral-100">Job List</h1>
+        <h1 className="heading-xxl-bold md:text-neutral-100">Job List</h1>
       </header>
-      <main className="flex flex-col gap-[24px] md:px-[32px] lg:px-[100px]">
+      <main className="flex flex-col gap-8 md:gap-6 px-4 md:px-8 lg:px-[100px]">
         <Search />
         <ul className="w-full grid md:grid-cols-2 lg:grid-cols-3 grid-flow-row gap-[24px] m-auto">
-          {vacancies.map((loker) => (
+          {vacancies.map((vacancy) => (
             <Card
-              key={loker.id}
-              title={loker.title}
-              type={loker.type}
-              category={loker.category}
-              place={loker.place}
+              key={vacancy.id}
+              name={vacancy.name}
+              type={vacancy.type}
+              category={vacancy.category}
+              place={vacancy.place}
               src={CyberSecurity}
-              id={loker.id}
+              id={vacancy.id}
             />
           ))}
         </ul>
       </main>
+      <section className="relative flex w-full items-center py-9 md:py-[60px] px-4 md:px-8 lg:px-[100px] justify-between">
+        <p className="static md:absolute">
+          Showing <b>12</b> of <b>23</b>
+        </p>
+        <div className="flex gap-1 p-1 text-l-regular md:mx-auto">
+          <button className="bg-primaryBlue-main text-neutral-100 rounded-lg p-1">
+            <p className=" w-[26px]">1</p>
+          </button>
+          <button className="text-neutral-1000 rounded-lg p-1 w-[26px]">
+            <p className=" w-[26px]">2</p>
+          </button>
+        </div>
+      </section>
     </section>
   );
 }
