b1c912769cf2f68240052492c127c5b65f38dfeb
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-04-06 22:37:04 +0800

    complete new dashboard

--- src/components/admin/VacancyCard.jsx
+++ src/components/admin/VacancyCard.jsx
@@ -1,39 +1,37 @@
-import * as Icon from "react-bootstrap-icons";
+import Pin from "assets/icons/pin.svg";
 
 export default function VacancyCard(props) {
   const {
-    vacancy: {
-      name,
-      company,
-      endDate,
-      applicantsCount: { total },
-    },
+    name,
+    endDate,
+    address,
+    type,
+    index,
+    applicantsCount: { total },
   } = props;
   return (
-    <tr className="hover:bg-slate-100 ">
-      <td className="pl-[27px] py-4 rounded-l-xl border-0">
-        <h1 className="font-rajdhani font-semibold text-md">{name}</h1>
-        <span className="mt-[12px] flex gap-1 items-center">
-          <Icon.PinFill size={10} />
-          <p className=" text-xs">{company}</p>
+    <tr className=" text-black">
+      <td className={`flex flex-col gap-3 pl-[27px] ${index === 0 ? "pt-[47px]": "pt-[28px"} py-4 rounded-l-xl border-0 font-inter`}>
+        <h1 className="heading-xs-medium">{name}</h1>
+        <span className="flex gap-1 items-center">
+          <img src={Pin} alt="pin" />
+          <p className="text-s-regular">{address}</p>
         </span>
-        <p className="mt-[12px] text-xs">Internship</p>
+        <p className="text-s-regular opacity-50">{type}</p>
       </td>
 
-      <td>
-        <h1 className="text-l-regular text-center">
-          {total}
-        </h1>
-        <p className="text-s-regular text-black text-opacity-50 text-center mt-[8px]">Pelamar</p>
+      <td className={index === 0 && "pt-[47px]"}>
+        <h1 className="text-l-regular text-center">{total}</h1>
+        <p className="text-s-regular text-black text-opacity-50 text-center mt-[8px]">
+          Pelamar
+        </p>
       </td>
 
-      <td>
-        <h1 className="text-m-regular text-center">
-          {endDate}
-        </h1>
+      <td className={index === 0 && "pt-[47px]"}>
+        <h1 className="text-m-regular text-center">{endDate}</h1>
       </td>
 
-      <td align="right" className="rounded-r-xl border-0 pr-[98px]">
+      <td align="right" className={`${index === 0 && "pt-[47px] "} rounded-r-xl border-0 pr-[98px]`}>
         <button className="text-blue-500 font-semibold text-sm cursor-pointer">
           Lihat Detail
         </button>
--- src/pages/admin/dashboard/index.jsx
+++ src/pages/admin/dashboard/index.jsx
@@ -6,7 +6,7 @@ import lamaran1 from "assets/icons/lamaran-1.svg";
 import lamaran2 from "assets/icons/lamaran-2.svg";
 import lamaran3 from "assets/icons/lamaran-3.svg";
 
-function Dashboard() {
+export default function AdminDashboard() {
   return (
     <div className="box-border flex-1 p-8 bg-[#fcfdfd] font-cairo flex flex-col gap-4">
       <h1 className="heading-l-bold mt-[44px]">Welcome to Admin Career Page</h1>
@@ -16,7 +16,9 @@ function Dashboard() {
         <Card count="10" title="Lamaran Sedang Diproses" icon={lamaran3} />
       </section>
       <section className="flex flex-col gap-[8px]">
-        <h1 className="px-[12px] heading-m-medium mt-[31px]">Lowongan belum diproses terbanyak</h1>
+        <h1 className="px-[12px] heading-m-medium mt-[31px]">
+          Lowongan belum diproses terbanyak
+        </h1>
         <div className="bg-white shadow rounded-xl p-[12px] w-full">
           <table className="w-full border-separate border-spacing-y-[4px]">
             <thead className="bg-primaryBlue-main text-neutral-100 rounded-[32px] heading-s-bold">
@@ -35,7 +37,7 @@ function Dashboard() {
                 .filter((vacancy) => vacancy.status === "Aktif")
                 .sort((a, b) => b.total - a.total)
                 .map((vacancy, index) => (
-                  <VacancyCard vacancy={vacancy} key={index} />
+                  <VacancyCard {...vacancy} key={index} index={index} />
                 ))}
             </tbody>
           </table>
@@ -44,5 +46,3 @@ function Dashboard() {
     </div>
   );
 }
-
-export default Dashboard;
