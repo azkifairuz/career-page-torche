6e3b0eac6e6285ca9360d291bd05c317e104ea84
azkifairuz <azkiajmal@gmail.com>
2023-04-08 23:54:14 +0700

    fetch number

--- src/components/User/Application Tracking/Pagination.js
+++ src/components/User/Application Tracking/Pagination.js
@@ -29,7 +29,50 @@ class Pagination extends Component {
     this.totalPages = Math.ceil(this.totalRecords / this.pageLimit);
     this.state = { currentPage: 1 };
   }
-  
+  fetchPageNumbers = () => {
+    const totalPages = this.totalPages;
+    const currentPage = this.state.currentPage;
+    const pageNeighbours = this.pageNeighbours;
+
+    const totalNumbers = (this.pageNeighbours * 2) + 3;
+    const totalBlocks = totalNumbers + 2;
+
+    if (totalPages > totalBlocks) {
+      const startPage = Math.max(2, currentPage - pageNeighbours);
+      const endPage = Math.min(totalPages - 1, currentPage + pageNeighbours);
+      let pages = range(startPage, endPage);
+
+      const hasLeftSpill = startPage > 2;
+      const hasRightSpill = (totalPages - endPage) > 1;
+      const spillOffset = totalNumbers - (pages.length + 1);
+
+      switch (true) {
+
+        case (hasLeftSpill && !hasRightSpill): {
+          const extraPages = range(startPage - spillOffset, startPage - 1);
+          pages = [LEFT_PAGE, ...extraPages, ...pages];
+          break;
+        }
+
+
+        case (!hasLeftSpill && hasRightSpill): {
+          const extraPages = range(endPage + 1, endPage + spillOffset);
+          pages = [...pages, ...extraPages, RIGHT_PAGE];
+          break;
+        }
+
+        case (hasLeftSpill && hasRightSpill):
+        default: {
+          pages = [LEFT_PAGE, ...pages, RIGHT_PAGE];
+          break;
+        }
+      }
+
+      return [1, ...pages, totalPages];
+    }
+
+    return range(1, totalPages);
+  }
 }
 
 Pagination.propTypes = {
