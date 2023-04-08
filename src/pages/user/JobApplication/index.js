76e33f81f5bd7baaf5e7f6656755b68a950a8905
azkifairuz <azkiajmal@gmail.com>
2023-04-09 02:49:46 +0700

    complete job application

--- src/pages/user/JobApplication/index.js
+++ src/pages/user/JobApplication/index.js
@@ -10,10 +10,12 @@ function JobApplication() {
             <div className="capitalize mt-14 mr-[100px] w-fit">
                 <div className="flex justify-between mb-16 w-ful">
                     <h1 className="heading-l-bold">Lamaran</h1>
-                    <select  className="border-2 flex flex-row items-center gap-11 text-xl-regular bg-primaryBlue-surface border-primaryBlue-border rounded-[4px] py-2 px-4 w-[160px]">
+                    <div>
+                        <select className="border-2  flex flex-row items-center gap-11 text-xl-regular bg-primaryBlue-surface border-primaryBlue-border rounded-[4px] py-2 px-4 w-[160px]">
                         <option>Terbaru</option>
                         <ChevronDown size={20} color={"#5885E9"}/>
                     </select>
+                    </div>
                 </div>
                 <div className="flex flex-col gap-6 w-full">
                         {applications.slice(0,5).map((data)=>{
