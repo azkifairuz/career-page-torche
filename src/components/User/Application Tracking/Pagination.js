aa103aca78152dd72dcca9e88f001100f1fd4883
azkifairuz <azkiajmal@gmail.com>
2023-04-08 23:52:36 +0700

    pages limit

---
+++ src/components/User/Application Tracking/Pagination.js
@@ -0,0 +1,42 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+const LEFT_PAGE = 'LEFT';
+const RIGHT_PAGE = 'RIGHT';
+
+const range = (from, to, step = 1) => {
+  let i = from;
+  const range = [];
+
+  while (i <= to) {
+    range.push(i);
+    i += step;
+  }
+
+  return range;
+}
+
+class Pagination extends Component {
+  constructor(props) {
+    super(props);
+    const { totalRecords = null, pageLimit = 3, pageNeighbours = 2 } = props;
+
+    this.pageLimit = typeof pageLimit === 'number' ? pageLimit : 3;
+    this.totalRecords = typeof totalRecords === 'number' ? totalRecords : 0;
+    this.pageNeighbours = typeof pageNeighbours === 'number'
+      ? Math.max(0, Math.min(pageNeighbours, 2))
+      : 0;
+    this.totalPages = Math.ceil(this.totalRecords / this.pageLimit);
+    this.state = { currentPage: 1 };
+  }
+  
+}
+
+Pagination.propTypes = {
+  totalRecords: PropTypes.number.isRequired,
+  pageLimit: PropTypes.number,
+  pageNeighbours: PropTypes.number,
+  onPageChanged: PropTypes.func
+};
+
+export default Pagination;
