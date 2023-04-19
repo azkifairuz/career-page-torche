1cc3a6e02d8976a33839d5faf2998007c367f2ce
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-04-19 23:23:12 +0800

    update pagination

--- src/components/molecules/Pagination.jsx
+++ src/components/molecules/Pagination.jsx
@@ -1,14 +1,30 @@
 import { useState } from "react";
+import { ChevronLeft, ChevronRight } from "react-bootstrap-icons";
 
 export default function Pagination(props) {
-  const [maxRow, setMaxRow] = useState(10);
   const { maxData } = props;
+  const [maxRow, setMaxRow] = useState(10);
+  const [maxPage, setMaxPage] = useState(Math.ceil(maxData / maxRow));
+  const [currentPage, setCurrentPage] = useState(1);
+
   const handleSelect = (e) => {
     e.preventDefault();
     setMaxRow(e.target.value);
+    setMaxPage(Math.ceil(maxData / e.target.value));
   };
+
+  const handleNextPage = (e) => {
+    e.preventDefault();
+    setCurrentPage(currentPage + 1);
+  };
+
+  const handlePrevPage = (e) => {
+    e.preventDefault();
+    setCurrentPage(currentPage - 1);
+  };
+
   return (
-    <section className="border-t-2 border-t-slate-200 text-black p-4 gap-4 flex justify-end items-center text-l-bold">
+    <section className="border-t-2 border-t-slate-200 text-black p-4 gap-10 flex justify-end items-center text-l-bold">
       <div className="flex items-center">
         <p>Tampilkan per Halaman: </p>
         <select className="p-2" value={maxRow} onChange={handleSelect}>
@@ -19,15 +35,19 @@ export default function Pagination(props) {
           <option value={50}>50</option>
         </select>
       </div>
-      <p>
-        {1} - {maxRow} of {maxData}
-      </p>
-      <div>
-        <button className="p-2 px-4 rounded-lg bg-blue-800 text-white">
-          Prev
+      <div className="flex gap-5">
+        <p>
+          {maxRow * (currentPage - 1) + 1} -{" "}
+          {currentPage * maxRow > maxData ? maxData : currentPage * maxRow} of{" "}
+          {maxData}
+        </p>
+        <button disabled={currentPage === 1} onClick={handlePrevPage}>
+          <ChevronLeft color={currentPage === 1 ? "#D8D8D8" : "#151515"} />
         </button>
-        <button className="p-2 px-4 rounded-lg bg-blue-800 text-white">
-          Next
+        <button disabled={currentPage === maxPage} onClick={handleNextPage}>
+          <ChevronRight
+            color={currentPage === maxPage ? "#D8D8D8" : "#151515"}
+          />
         </button>
       </div>
     </section>
