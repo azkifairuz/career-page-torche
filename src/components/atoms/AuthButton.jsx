68433af7f24af09e11c683914e1e7b2ac30927d8
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-04-24 20:50:52 +0800

    fix:variable name consistency

--- src/components/atoms/AuthButton.jsx
+++ src/components/atoms/AuthButton.jsx
@@ -1,8 +1,8 @@
 export default function AuthButton(props) {
-  const { handleClick, name, type = "button", title } = props;
+  const { onClick, name, type = "button", title } = props;
   return (
     <button
-      onClick={handleClick}
+      onClick={onClick}
       name={name}
       type={type}
       className="w-full bg-primaryBlue-main hover:bg-primaryBlue-hover active:bg-primaryBlue-pressed  text-white focus:outline-none px-[14px] py-[16px] rounded-[8px] "
