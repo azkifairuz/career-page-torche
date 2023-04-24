0988396e9e391257e65105a8ad7a30453f00c21c
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-04-24 21:46:02 +0800

    complete verify page

--- src/pages/signup/verify.jsx
+++ src/pages/signup/verify.jsx
@@ -2,8 +2,17 @@ import AuthContainer from "components/organisms/AuthContainer";
 import InputField from "components/atoms/InputField";
 
 import Background from "assets/images/RegisterBG.webp";
+import AuthButton from "components/atoms/AuthButton";
+import { useNavigate } from "react-router-dom";
 
 export default function Verify() {
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/login");
+  }
+
   return (
     <main className="flex p-0 md:px-[100px] md:py-[62px] justify-center lg:justify-end items-center min-h-screen">
       <img
@@ -12,21 +21,17 @@ export default function Verify() {
         className="fixed -z-10 top-0 left-0 min-h-screen"
       />
       <AuthContainer>
-        <div className="flex flex-col gap-[16px] w-full h-fit ">
-          <p className="text-neutral-1000">
-            We just sent you a temporary sign up code. Please check your inbox
-            and paste the sign up code below.
-          </p>
-          <div className="flex flex-col gap-6 bg-red-50">
-            <InputField
-              label="Kode Verifikasi"
-              type="text"
-              placeholder="••••••••"
-            />
-            <button type="submit" className="w-full">
-              Verifikasi
-            </button>
-          </div>
+        <p className="text-neutral-1000">
+          We just sent you a temporary sign up code. Please check your inbox and
+          paste the sign up code below.
+        </p>
+        <div className="flex flex-col gap-6 w-full">
+          <InputField
+            label="Kode Verifikasi"
+            type="text"
+            placeholder="••••••••"
+          />
+          <AuthButton title="Verifikasi" type="submit" onClick={handleSubmit}/>
         </div>
       </AuthContainer>
     </main>
