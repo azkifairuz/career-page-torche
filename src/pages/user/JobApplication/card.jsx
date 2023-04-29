3c0481266ce5eb1543a7e622f75b4e19f680d923
azkifairuz <azkiajmal@gmail.com>
2023-04-29 10:56:39 +0700

    fix:button

--- src/pages/user/JobApplication/card.jsx
+++ src/pages/user/JobApplication/card.jsx
@@ -7,11 +7,15 @@ export default function Card(props) {
     });
   return (
     <div className="p-4 w-[284px] md:w-[422px] flex shadow-[0px_0px_10px_0px_#0000001A] bg-neutral-100 gap-3 flex-col justify-between md:flex-row  ">
-        <div className="flex flex-col justify-between">
-          <h1 className="uppercase font-bold font-rajdhani text-[24px] leading-7">
-            {posisi}
-          </h1>
-          <div />
+        <div className="flex flex-col w-full ">
+          <div className="flex justify-between ">
+            <h1 className="uppercase font-bold font-rajdhani text-[24px] leading-7">
+                {posisi}
+            </h1>
+            <button className="py-2 hidden md:block h-fit rounded-lg px-6  bg-primaryBlue-main font-cairo text-white capitalize active:bg-primaryBlue-pressed hover:bg-primaryBlue-hover">
+            details
+            </button>
+        </div>
           <div className="flex flex-col mt-4">
             <p className="text-m-bold text-primaryBlue-main">Torche App</p>
             <p className="text-m-regular">{place}</p>
@@ -37,8 +41,8 @@ export default function Card(props) {
             <h1 className="text-s-regular self-end">Diikirim : {tanggal}</h1>
           </div>
         </div>
-         <div>
-            <button className="py-2 rounded-lg px-6 w-full bg-primaryBlue-main font-cairo text-white capitalize active:bg-primaryBlue-pressed hover:bg-primaryBlue-hover">
+        <div className="md:hidden">
+            <button className="py-2 h-fit  rounded-lg px-6 w-full bg-primaryBlue-main font-cairo text-white capitalize active:bg-primaryBlue-pressed hover:bg-primaryBlue-hover">
             details
             </button>
         </div>
