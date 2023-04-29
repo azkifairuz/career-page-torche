b0d6c7d66da3b8cbbe1dfd49a8617d1c9ec524b0
azkifairuz <azkiajmal@gmail.com>
2023-04-29 10:40:25 +0700

    feat:tablet responsive

--- src/pages/user/JobApplication/card.jsx
+++ src/pages/user/JobApplication/card.jsx
@@ -6,7 +6,7 @@ export default function Card(props) {
         categories.push({element})
     });
   return (
-    <div className="p-4 w-[284px] flex shadow-[0px_0px_10px_0px_#0000001A] bg-neutral-100 gap-3 flex-col justify-between lg:w-[830px]">
+    <div className="p-4 w-[284px] md:w-[422px] flex shadow-[0px_0px_10px_0px_#0000001A] bg-neutral-100 gap-3 flex-col justify-between md:flex-row  ">
         <div className="flex flex-col justify-between">
           <h1 className="uppercase font-bold font-rajdhani text-[24px] leading-7">
             {posisi}
@@ -37,7 +37,7 @@ export default function Card(props) {
             <h1 className="text-s-regular self-end">Diikirim : {tanggal}</h1>
           </div>
         </div>
-      <div>
+         <div>
             <button className="py-2 rounded-lg px-6 w-full bg-primaryBlue-main font-cairo text-white capitalize active:bg-primaryBlue-pressed hover:bg-primaryBlue-hover">
             details
             </button>
