19a469279bf41849e2dfd81222e33926d7f73ca9
azkifairuz <azkiajmal@gmail.com>
2023-04-30 14:11:39 +0700

    responsive application tracking

--- src/pages/user/dashboard/application track/Card.jsx
+++ src/pages/user/dashboard/application track/Card.jsx
@@ -1,24 +1,40 @@
 
 export default function Card(props) {
-    const {posisi,salary,place,tanggal,status}=props
+    const { posisi, salary, status, tanggal } = props;
 
-
-
-    return(
-        <div className="py-6 px-8 flex shadow-[0px_0px_8px_0px_#00000033] bg-neutral-100 gap-3 flex-col justify-between lg:w-[830px] w-full">
-           <div className="flex justify-between">
-           <h1 className="uppercase font-bold font-rajdhani text-[24px] leading-7">{posisi}</h1>
-           <button className="py-2 rounded-lg px-6 bg-primaryBlue-main font-cairo text-white capitalize active:bg-primaryBlue-pressed hover:bg-primaryBlue-hover">details</button>
-           </div>
-            <div className="flex gap-[23px]">
-                <p className="text-m-bold text-primaryBlue-main">Torche App</p>
-                <p className="text-m-regular">{place}</p>
+    return (
+      <div className="p-4 md:min-w-[422px]  flex shadow-[0px_0px_10px_0px_#0000001A] bg-neutral-100 gap-3 flex-col justify-between md:flex-row  ">
+          <div className="flex flex-col w-full ">
+            <div className="flex justify-between ">
+              <h1 className="uppercase font-bold font-rajdhani text-[24px] leading-7">
+                  {posisi}
+              </h1>
+              <button className="py-2 hidden md:block h-fit rounded-lg px-6  bg-primaryBlue-main font-cairo text-white capitalize active:bg-primaryBlue-pressed hover:bg-primaryBlue-hover">
+              details
+              </button>
+          </div>
+            <div className="flex flex-col mt-4">
+              <p className="text-m-bold text-primaryBlue-main">Torche App</p>
+              <p className="text-m-regular">Jakarta,Indonesia</p>
             </div>
-            <h1 className="uppercase text-l-bold text-primaryBlue-main">{salary}</h1>
-            <div className="flex justify-between">
-                <div className="text-m-regular w-fit py-1 px-2 capitalize text-primaryBlue-main bg-primaryBlue-surface border-black">{status}</div>
-                <h1 className="text-s-regular">Diikirim : {tanggal}</h1>
+            <h1 className="uppercase text-l-bold mt-2 text-primaryBlue-main">
+              {salary}
+            </h1>
+            <div className="grid grid-cols-2 lg:flex w-fit gap-[5px] mt-4">
+              <div className="text-s-regular justify-self-start w-fit text-center py-2 px-3 capitalize text-primaryBlue-main rounded-lg bg-primaryBlue-surface border-black">
+                  {status}
+              </div>
             </div>
-        </div>
-    )
-}
+            <div className="flex flex-col mt-1  ">
+              <h1 className="text-s-regular self-end lg:self-start text-neutral-700">Diikirim : {tanggal}</h1>
+            </div>
+          </div>
+          <div className="md:hidden">
+              <button className="py-2 h-fit  rounded-lg px-6 w-full bg-primaryBlue-main font-cairo text-white capitalize active:bg-primaryBlue-pressed hover:bg-primaryBlue-hover">
+              details
+              </button>
+          </div>
+      </div>
+    );
+  }
+  
--- src/pages/user/dashboard/application track/index.jsx
+++ src/pages/user/dashboard/application track/index.jsx
@@ -9,10 +9,10 @@ function Tracking() {
      
         
          return (
-            <div className="capitalize p-4">
-                <div className="flex justify-between mb-16 w-ful">
+            <div className="capitalize flex flex-col shadow-[0px_0px_10px_0px_#0000001A]  gap-4  p-6 md:px-[42px] md:py-8 ">
+                <div className="flex justify-between border-b pb-2 border-primaryBlue-border w-full">
                     <h1 className="heading-l-bold">status lamaran</h1>
-                    <select  className="border-2 flex flex-row items-center gap-11 text-xl-regular bg-primaryBlue-surface border-primaryBlue-border rounded-[4px] py-2 px-4 w-[160px]">
+                    <select  className="border-2 text-xl-regular text-neutral-900 w-[108px] flex flex-row items-center gap-11 text-xl-regular bg-primaryBlue-surface border-primaryBlue-border rounded-[4px] py-1 px-2 ">
                         <option>Terbaru</option>
                         <ChevronDown size={20} color={"#5885E9"}/>
                     </select>
@@ -20,7 +20,7 @@ function Tracking() {
                 <div className="flex flex-col gap-6 w-full">
                         {applications.slice(0,5).map((data)=>{
                         return(
-                            <Card posisi={data.posisi} status={data.status} isClosed={data.isClosed} tanggal={data.tanggal} perusahaan={data.perusahaan} salary={data.salary}/>
+                            <Card posisi={data.posisi} status={data.status} isClosed={data.isClosed} tanggal={data.tglDikirim} perusahaan={data.perusahaan} salary={data.salary}/>
                         )
                     })}
                 </div>
