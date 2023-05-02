ae99a7cc87954f9c14605e3bd30c0091e833bf67
azkifairuz <azkiajmal@gmai.com>
2023-05-02 20:55:13 +0700

    avatar in navbar

--- src/components/User/navbar.js
+++ src/components/User/navbar.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import notificationBadge from "assets/icons/notification-badge.svg"
 import Logo from "../../assets/logos/Torche_Logo-01_White.webp";
-
+import sample from "assets/index.png";
 export default function Navbar() {
   const [isNavbarOpen, setIsNavbarOpen] = useState(false);
 
@@ -61,10 +61,13 @@ export default function Navbar() {
             </ul>
           </div>
         </div>
-        <div className="md:flex hidden justify-center">
-          <div className="relative w-[28px] h-[28px]">
+        <div className="md:flex hidden justify-center items-center">
+            <div className="relative w-[28px] h-[28px]">
                 <span className={`absolute right-0 -top-1  bg-danger-main w-[12px] h-[12px] flex justify-center items-center font-cairo text-[9.6px] p-[6.4px] text-center leading-[17.6px] rounded-full ${notification.length >0  ? "flex":"hidden"} `}>{notification.length}</span>
                 <img src={notificationBadge} className="w-[17.7px] h-[22.75px]"></img>
+            </div>
+            <div>
+              <img src={sample} className="w-[48px] h-[48px] rounded-full" />
             </div>
           </div>
       </nav>
