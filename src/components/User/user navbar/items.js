6bd7cdf3c9347442f536c20b89160aa0a54e9067
azkifairuz <azkiajmal@gmail.com>
2023-05-09 23:31:26 +0700

    create navbar user folder

---
+++ src/components/User/user navbar/items.js
@@ -0,0 +1,22 @@
+import { Link} from "react-router-dom";
+
+export default function Item(props) {
+  const { icon, title, href,isActive, setIsActive,index } = props;
+  const activeLink = "border-primaryBlue-main border-l-2 border-primaryBlue-main text-primaryBlue-main"
+  const normal = ""
+  
+  return (
+    <Link
+      onClick={
+        ()=>{
+          setIsActive(index)
+        }
+      }
+      to={href}
+      className={`flex items-center w-full  gap-5 font-cairo hover:font-bold text-m-regular hover:border-l-2 hover:border-primaryBlue-main hover:text-primaryBlue-main hover:bg-primaryBlue-surface active:text-primaryBlue-main active:bg-primaryBlue-surface text-[#494949]  p-4 ${isActive ? activeLink : normal  } `}
+    >
+      {icon}
+      <span>{title}</span>
+    </Link>
+  );
+}
