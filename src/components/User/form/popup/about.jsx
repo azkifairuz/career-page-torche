82d5da1830d35f568282a8e13433d073553aa516
azkifairuz <azkiajmal@gmai.com>
2023-05-10 20:40:30 +0700

    about popup header

---
+++ src/components/User/form/popup/about.jsx
@@ -0,0 +1,9 @@
+export default function AboutPopUp(params) {
+    return (
+        <div className="rounded-lg">
+            <div className="p-[16px_32px_16px_32px] border border-neutral-400 flex justify-between">
+                <h1 className="">Tentang Saya</h1>
+            </div>
+        </div>
+    )
+}
