8b218cdf457dfec331400470a66a772edd8c8bc3
azkifairuz <azkiajmal@gmai.com>
2023-05-10 22:04:27 +0700

    about popup rounded field

--- src/components/User/Complete Profile/popup/aboutPopup.jsx
+++ src/components/User/Complete Profile/popup/aboutPopup.jsx
@@ -10,7 +10,7 @@ export default function AboutPopUp(params) {
                 <p className="text-l-regular text-neutral-900">Jelaskan singkat tentang dirimu agar perekrut dapat dengan mudah melihat kualifikasimu.</p>
                 <textarea 
                 type="text"
-                className="h-[180px] border border-neutral-900 w-full resize-none outline-1"> 
+                className="h-[180px] border rounded-[4px] px-[16px] py-2 border-neutral-700 w-full resize-none outline-1"> 
 
                 </textarea>
             </div>
