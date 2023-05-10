55e0aec95fea0ec377c342d629112fc5935e1ff8
azkifairuz <azkiajmal@gmail.com>
2023-05-10 20:01:51 +0700

    fix handle profil nav

--- src/pages/user/JobApplication/index.jsx
+++ src/pages/user/JobApplication/index.jsx
@@ -1,35 +1,40 @@
-import Card from "./card"
-import { applications } from "data/application"
-import Pagination from "components/User/Pagination"
+import Card from "./card";
+import { applications } from "data/application";
+import Pagination from "components/User/Pagination";
 
 function JobApplication() {
-     
-        
-         return (
-            <div className="capitalize flex flex-col shadow-[0px_0px_10px_0px_#0000001A]  gap-4  p-6 md:px-[42px] md:py-8 ">
-                <div className="flex justify-between border-b pb-2 border-primaryBlue-border w-full">
-                    <h1 className="heading-m-bold md:heading-l-bold">Lamaran</h1>
-                    <div>
-                    <select className={`appearance-none bg-no-repeat bg-right bg-[url(assets/icons/chevron-down.svg)] border-2  text-xl-regular text-neutral-900 w-[108px] flex flex-row items-center gap-11 text-xl-regular bg-primaryBlue-surface border-primaryBlue-border rounded-[4px] py-1 px-2`}>
-                        <option>Terbaru</option>
-                    </select>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-6 w-full">
-                        {applications.slice(0,5).map((data)=>{
-                        return(
-                            <Card posisi={data.posisi} place={data.place}  isClosed={data.isClosed} tanggal={data.tglDikirim} perusahaan={data.perusahaan} salary={data.salary} category={data.category}/>
-                        )
-                    })}
-                </div>
-                <div>
-                    
-                </div>
-                <div>
-                    <Pagination/>
-                </div>
-            </div>
-           
-         )
+  return (
+    <div className="capitalize flex flex-col shadow-[0px_0px_10px_0px_#0000001A]  gap-4  p-6 md:px-[42px] md:py-8 ">
+      <div className="flex justify-between border-b pb-2 border-primaryBlue-border w-full">
+        <h1 className="heading-m-bold md:heading-l-bold">Lamaran</h1>
+        <div>
+          <select
+            className={`appearance-none bg-no-repeat bg-right bg-[url(assets/icons/chevron-down.svg)] border-2  text-xl-regular text-neutral-900 w-[108px] flex flex-row items-center gap-11 text-xl-regular bg-primaryBlue-surface border-primaryBlue-border rounded-[4px] py-1 px-2`}
+          >
+            <option>Terbaru</option>
+          </select>
+        </div>
+      </div>
+      <div className="flex flex-col gap-6 w-full">
+        {applications.slice(0, 5).map((data) => {
+          return (
+            <Card
+              posisi={data.posisi}
+              place={data.place}
+              isClosed={data.isClosed}
+              tanggal={data.tglDikirim}
+              perusahaan={data.perusahaan}
+              salary={data.salary}
+              category={data.category}
+            />
+          );
+        })}
+      </div>
+      <div></div>
+      <div>
+        <Pagination />
+      </div>
+    </div>
+  );
 }
-export default JobApplication
+export default JobApplication;
