551bd23da98329a68baf1fe7f28002d3d3808474
azkifairuz <azkiajmal@gmai.com>
2023-05-14 00:49:58 +0700

    fix route resume page

--- src/components/organisms/userSidebar/index.jsx
+++ src/components/organisms/userSidebar/index.jsx
@@ -53,7 +53,7 @@ export default function Sidebar(props) {
     },
     {
       title: "Resume",
-      href: "/user/ompleteprofile",
+      href: "/user/completeprofile",
       icon: resumeIcon,
     },
     {
