113d0692d55d6657e6c0b828629651ded84537e4
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-05-14 01:12:02 +0800

    fixing text

--- src/pages/about/Card.jsx
+++ src/pages/about/Card.jsx
@@ -4,7 +4,7 @@ export default function Card(props) {
     <div className="flex flex-col gap-[40px] items-center grow basis-1/4">
       <img src={icon} alt="icon" className="w-[80px]" />
       <section className="flex flex-col gap-[16px] items-center">
-        <h3 className="heading-s-bold">{title}</h3>
+        <h3 className="heading-s-bold text-center">{title}</h3>
         <p className="text-xl-regular text-justify">{description}</p>
       </section>
     </div>
