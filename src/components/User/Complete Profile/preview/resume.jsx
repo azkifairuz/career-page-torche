c42c65fb1b128b3bff7700eb88cd2c9cd23c642c
azkifairuz <azkiajmal@gmai.com>
2023-05-14 00:18:48 +0700

    handle resume card hidden

--- src/components/User/Complete Profile/preview/resume.jsx
+++ src/components/User/Complete Profile/preview/resume.jsx
@@ -1,22 +1,31 @@
 import { PlusCircle } from "react-bootstrap-icons";
-
+import { useState } from "react";
+import ResumePopup from "../popup/resumePopup";
 export default function Resume(props) {
-
-        return (
-            <div className=" w-full flex flex-col gap-4 shadow-card rounded-lg p-6">
-            
-                <div className="flex justify-between">
-                    <h1 className="heading-s-bold font-rajdhani">Resume</h1>
-                    <div
-                        className="flex items-center gap-2 py-1 px-2 cursor-pointer text-white w-fit bg-primaryBlue-main rounded">
-                        <PlusCircle size={12} />
-                        <p className="font-cairo text-s-regular">Tambah</p>
-                    </div>
-                </div>
-                <div>
-                    <h1 className="mb-2 text-l-bold font-cairo text-neutral-1000">Resume</h1>
-                    <p className="text-l-regular font-cairo text-primaryBlue-main">cv.Resume.pdf</p>
+    const [isCardOpen, setIsCardOpen] = useState(false)
+    const handleCardOpen = () => {
+        setIsCardOpen(!isCardOpen)
+        console.log(isCardOpen);
+    }
+    return (
+        <div className=" w-full flex flex-col gap-4 shadow-card rounded-lg p-6">
+            <div className={` ${!isCardOpen && "hidden"}   fixed flex p-52 inset-0 overflow-scroll justify-center items-center  z-50 bg-opacity-50 w-full   bg-black`}>
+                <div className="rounded-lg w-fit mt-[300px]">
+                    <ResumePopup onclick={handleCardOpen} />
                 </div>
             </div>
-        )
-    }
+            <div className="flex justify-between">
+                <h1 className="heading-s-bold font-rajdhani">Resume</h1>
+                <div
+                    className="flex items-center gap-2 py-1 px-2 cursor-pointer text-white w-fit bg-primaryBlue-main rounded">
+                    <PlusCircle size={12} />
+                    <p className="font-cairo text-s-regular">Tambah</p>
+                </div>
+            </div>
+            <div>
+                <h1 className="mb-2 text-l-bold font-cairo text-neutral-1000">Resume</h1>
+                <p className="text-l-regular font-cairo text-primaryBlue-main">cv.Resume.pdf</p>
+            </div>
+        </div>
+    )
+}
