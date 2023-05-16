4824b62a67d31ab3822b787528408b24e4a550a1
azkifairuz <azkiajmal@gmai.com>
2023-05-16 21:53:45 +0700

    handle toggle

--- src/components/atoms/ToggleSwicth.jsx
+++ src/components/atoms/ToggleSwicth.jsx
@@ -2,7 +2,27 @@ import React, { useState } from 'react';
 
 export default function ToggleSwitch(props) {
     const [isActive,setIsactive] = props
-    const handlerCLick = ()=>{
+    const handleCLick = ()=>{
         setIsactive(!isActive)
     }
+
+    return(
+        <div className="relative inline-block w-10 align-middle select-none transition duration-200 ease-in">
+            <input
+            id='toggle'
+            className={`toggle-checkbox absolute block w-5 h-5 border-none appearance-none cursor-pointer rounded-full bg-neutral-700
+            ${isActive ?  "bg-primaryBlue-main" : "bg-neutral-700"}
+          `}
+            type='checkbox'
+            onChange={handleCLick}
+            checked = {isActive}
+            />
+            <label
+            htmlFor='toggle'
+            className={`block overflow-hidden p-[1px_4px_1px_4px] rounded-full bg-neutral-400 cursor-pointer
+                 ${isActive? "bg-primaryBlue-border": "bg-neutral-400"}
+            `}
+            />
+        </div>
+    )
 }
