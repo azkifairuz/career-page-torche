cfbfa7175102a5d3d1cd722e6e44956dab0e67a5
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-05-17 23:33:50 +0800

    fix unnecessary rule

--- src/pages/about/Card.jsx
+++ src/pages/about/Card.jsx
@@ -1,7 +1,7 @@
 export default function Card(props) {
   const { title, description, icon } = props
   return (
-    <div className="flex shrink-0 w-[219px] h-fit overflow-y-hidden md:w-auto md:flex flex-col gap-[40px] items-center md:shrink md:basis-1/4 md:grow">
+    <div className="flex shrink-0 w-[219px] h-fit overflow-y-hidden md:w-auto flex-col gap-[40px] items-center md:shrink md:basis-1/4 md:grow">
       <img src={icon} alt="icon" className="w-[80px] mx-auto" />
       <section className="flex flex-col gap-[16px] items-center">
         <h3 className="heading-s-bold text-center">{title}</h3>
