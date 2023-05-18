8691c1d28257cbb24f32346493712676e6981184
azkifairuz <azkiajmal@gmai.com>
2023-05-18 17:41:05 +0700

    feat:label component

--- src/data/application.js
+++ src/data/application.js
@@ -2,61 +2,61 @@ export const applications = [
     {
         id:1,
         posisi : "UI/UX DESIGNER",
-        place: "Remote",
+        place: "Fulltime",
         salary : "IDR 8,000,000 - IDR 10,000,000",
-        status: "Dalam Review",
+        status: "Interview",
         tglDikirim: "1 Maret 2023",
         category:["UI/UX designer","hardware & software"]
     },
     {
         id:2,
         posisi : "UI/UX DESIGNER",
-        place: "Remote",
+        place: "Fulltime",
         salary : "IDR 8,000,000 - IDR 10,000,000",
-        status: "terkirim",
+        status: "Tidak Lanjut",
         tglDikirim: "1 Maret 2023",
         category:["UI/UX designer","hardware & software"]
     },
     {
         id:3,
         posisi : "PYTHON ENGINEER",
-        place: "Remote",
+        place: "Fulltime",
         salary : "IDR 8,000,000 - IDR 10,000,000",
-        status: "diterima",
+        status: "Screening Test",
         tglDikirim: "1 Maret 2023",
         category:["Python Enginer","hardware & software"]
     },
     {
         id:4,
         posisi : "front end engineer",
-        place: "Remote",
+        place: "Fulltime",
         salary : "IDR 8,000,000 - IDR 10,000,000",
-        status: "Dalam Review",
+        status: "Offering",
         tglDikirim: "1 Maret 2023",
         category:["UI/UX designer","hardware & software"]
     },
     {
         id:5,
         posisi : "backend engineer",
-        place: "Remote",
+        place: "Fulltime",
         salary : "IDR 8,000,000 - IDR 10,000,000",
-        status: "Dalam Review",
+        status: "Offering",
         tglDikirim: "1 Maret 2023",
         category:["UI/UX designer","hardware & software"]
     },
     {
         id:6,
         posisi : "UI/UX DESIGNER",
-        place: "Remote",
+        place: "Internship",
         salary : "IDR 8,000,000 - IDR 10,000,000",
-        status: "Dalam Review",
+        status: "Offering",
         tglDikirim: "1 Maret 2023",
         category:["UI/UX designer","hardware & software"]
     },
     {
         id:7,
         posisi : "UI/UX DESIGNER",
-        place: "Remote",
+        place: "Internship",
         salary : "IDR 8,000,000 - IDR 10,000,000",
         status: "terkirim",
         tglDikirim: "1 Maret 2023",
@@ -65,7 +65,7 @@ export const applications = [
     {
         id:8,
         posisi : "PYTHON ENGINEER",
-        place: "Remote",
+        place: "Internship",
         salary : "IDR 8,000,000 - IDR 10,000,000",
         status: "diterima",
         tglDikirim: "1 Maret 2023",
@@ -74,34 +74,34 @@ export const applications = [
     {
         id:9,
         posisi : "front end engineer",
-        place: "Remote",
+        place: "Internship",
         salary : "IDR 8,000,000 - IDR 10,000,000",
-        status: "Dalam Review",
+        status: "Offering",
         tglDikirim: "1 Maret 2023",
         category:["UI/UX designer","hardware & software"]
     },
     {
         id:10,
         posisi : "backend engineer",
-        place: "Remote",
+        place: "Internship",
         salary : "IDR 8,000,000 - IDR 10,000,000",
-        status: "Dalam Review",
+        status: "Offering",
         tglDikirim: "1 Maret 2023",
         category:["UI/UX designer","hardware & software"]
     },
     {
         id:11,
         posisi : "Fe",
-        place: "Remote",
+        place: "Internship",
         salary : "IDR 8,000,000 - IDR 10,000,000",
-        status: "Dalam Review",
+        status: "Offering",
         tglDikirim: "1 Maret 2023",
         category:["UI/UX designer","hardware & software"]
     },
     {
         id:12,
         posisi : "BE",
-        place: "Remote",
+        place: "Internship",
         salary : "IDR 8,000,000 - IDR 10,000,000",
         status: "terkirim",
         tglDikirim: "1 Maret 2023",
@@ -110,7 +110,7 @@ export const applications = [
     {
         id:13,
         posisi : "PYTHON ENGINEER",
-        place: "Remote",
+        place: "Internship",
         salary : "IDR 8,000,000 - IDR 10,000,000",
         status: "diterima",
         tglDikirim: "1 Maret 2023",
@@ -119,18 +119,18 @@ export const applications = [
     {
         id:14,
         posisi : "front end engineer",
-        place: "Remote",
+        place: "Internship",
         salary : "IDR 8,000,000 - IDR 10,000,000",
-        status: "Dalam Review",
+        status: "Offering",
         tglDikirim: "1 Maret 2023",
         category:["UI/UX designer","hardware & software"]
     },
     {
         id:15,
         posisi : "backend engineer",
-        place: "Remote",
+        place: "Internship",
         salary : "IDR 8,000,000 - IDR 10,000,000",
-        status: "Dalam Review",
+        status: "Offering",
         tglDikirim: "1 Maret 2023",
         category:["UI/UX designer","hardware & software"]
     },
---
+++ src/pages/user/dashboard/application track/Label.jsx
@@ -0,0 +1,32 @@
+export default function name(text,status) {
+    switch (status) {
+        case 'Interview':
+          backgroundColor = 'bg-secondary-tosca';
+          textColor = '#151515';
+          break;
+        case 'Tidak Lanjut':
+          backgroundColor = 'bg-primaryBlue-surface';
+          textColor = 'text-neutral-600';
+          break;
+        case 'Screening Test':
+          backgroundColor = 'bg-primaryBlue-surface';
+          textColor = 'text-primaryBlue-main';
+          break;
+        case 'Offering':
+          backgroundColor = 'bg-secondary-purple';
+          textColor = 'text-white';
+          break;
+        default:
+          backgroundColor = 'bg-white';
+          break;
+      }
+
+      return(
+        <span
+        className={`px-8 py-2 rounded-8  font-roboto text-[15px] leading-[22.5px] text-center ${backgroundColor} ${textColor}`}
+        style={{ backgroundColor }}
+      >
+        {text}
+      </span>
+      )
+} 
