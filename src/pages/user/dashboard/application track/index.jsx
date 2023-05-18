31db9110b94af5b4b0b682163f8a93a5cef51cac
azkifairuz <azkiajmal@gmai.com>
2023-05-18 17:04:02 +0700

    fix gao

--- src/pages/user/dashboard/application track/index.jsx
+++ src/pages/user/dashboard/application track/index.jsx
@@ -9,7 +9,8 @@ function Tracking() {
      
         
          return (
-            <div className="capitalize flex flex-col shadow-[0px_0px_10px_0px_#0000001A]  gap-4  p-6 md:px-[42px] md:py-8 ">
+            <div className="flex flex-col gap-[24px]">
+                <div className="capitalize flex flex-col shadow-[0px_0px_10px_0px_#0000001A]  gap-4  p-6 md:px-[42px] md:py-8 ">
                 <div className="flex justify-between items-center border-b pb-2 border-primaryBlue-border w-full">
                     <h1 className="heading-s-bold md:heading-l-bold">status lamaran</h1>
                     <select className={`appearance-none bg-no-repeat bg-right bg-[url(assets/icons/chevron-down.svg)] border-2  text-xl-regular text-neutral-900 w-[108px] flex flex-row items-center gap-11 text-xl-regular bg-primaryBlue-surface border-primaryBlue-border rounded-[4px] py-1 px-2`}>
@@ -23,11 +24,13 @@ function Tracking() {
                         )
                     })}
                 </div>
-                <div>
-                    <Pagination/>
-                </div>
+                
             </div>
+            <div className="flex justify-center">
+            <Pagination/>
+        </div>
            
+            </div>
          )
 }
 export default Tracking
