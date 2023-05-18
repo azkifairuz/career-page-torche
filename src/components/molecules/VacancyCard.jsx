2cbe1ae2ad6faa45e59956a2fdf2bfb8f4f38cbe
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-05-18 18:31:38 +0800

    fix:flexible typography

--- src/components/molecules/VacancyCard.jsx
+++ src/components/molecules/VacancyCard.jsx
@@ -16,7 +16,7 @@ export default function VacancyCard(props) {
           index === 0 ? "pt-12" : "pt-7"
         } rounded-l-xl border-0 font-inter`}
       >
-        <h1 className="heading-xs-medium">{name}</h1>
+        <h1 className="text-m-regular md:text-l-regular">{name}</h1>
         <span className="flex gap-1 items-center">
           <img src={Pin} alt="pin" />
           <p className="text-s-regular">{address}</p>
@@ -25,21 +25,21 @@ export default function VacancyCard(props) {
       </td>
 
       <td className={index === 0 && "pt-12"}>
-        <h1 className="text-l-regular text-center">{total}</h1>
+        <h1 className="text-m-regular md:text-l-regular text-center">{total}</h1>
         <p className="text-s-regular text-black text-opacity-50 text-center mt-[8px]">
           Pelamar
         </p>
       </td>
 
       <td className={` hidden md:table-cell ${index === 0 && "pt-12"}`}>
-        <h1 className="text-m-regular text-center">{endDate}</h1>
+        <h1 className="text-l-regular text-center">{endDate}</h1>
       </td>
 
       <td
         align="center"
         className={`${index === 0 && "pt-12"} rounded-r-xl border-0 pr-8 `}
       >
-        <button className="text-blue-500 font-semibold text-sm cursor-pointer  ">
+        <button className="text-blue-500 font-semibold text-m-regular cursor-pointer  ">
           Lihat Detail
         </button>
       </td>
