3bd50a3ecc6bde16b300add40b6f23e3ba9b07cd
azkifairuz <azkiajmal@gmail.com>
2023-05-19 20:53:51 +0700

    link card responsive position

--- src/pages/complete profile/CompleteProfile.jsx
+++ src/pages/complete profile/CompleteProfile.jsx
@@ -12,7 +12,7 @@ export default function CompleteProfil() {
     <div className="flex flex-col mb-[72px]">
       {<HeaderSection />}
       <div className="md:flex w-full md:px-[32px]  lg:px-[100px]">
-        <div className="mt-6 py-4">
+        <div className="mt-6 py-4 ">
           <OptionBar />
         </div>
        
