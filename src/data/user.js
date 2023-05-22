fe58629ec56f689c4b6e13fa051e4df8e0c3ea03
azkifairuz <azkiajmal@gmai.com>
2023-05-22 21:16:42 +0700

    fix responsive

--- src/data/user.js
+++ src/data/user.js
@@ -4,7 +4,7 @@ export const user = {
     gender:"",
     dateBirth:"03/08/2003",
     description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.Lorem sadasd asdasd  dsad asd ",
-    phone:"+ 62 8132 1234 52",
+    phone:"8132 1234 52",
     email:"john@gmail.com",
     country:"indonesia",
     city:"jakarta",
--- src/pages/complete profile/CompleteProfile.jsx
+++ src/pages/complete profile/CompleteProfile.jsx
@@ -1,13 +1,12 @@
 import HeaderSection from "components/User/Complete Profile/header";
 import OptionBar from "components/User/Complete Profile/option bar";
 import Preview from "components/User/Complete Profile/preview";
-import { useState } from "react";
 export default function CompleteProfil() {
   return (
     <div className="flex flex-col mb-[72px]">
       {<HeaderSection />}
-      <div className="md:flex w-full md:px-[32px]  lg:px-[100px]">
-        <div className="mt-6 py-4 ">
+      <div className="md:flex w-full md:px-[32px]  lg:px-[100px] transition-all duration-500 ease-in-out   ">
+        <div className="mt-6 py-4 lg:w-[397px] ">
           <OptionBar />
         </div>
        
