4bb6466bee4fdb3d39d1e9784d15f377dcf8433c
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-06-03 01:15:45 +0800

    feat:label text

--- src/components/atoms/InputField.jsx
+++ src/components/atoms/InputField.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 export default function InputField(props) {
-  const { title, type, placeholder, value, onChange } = props;
+  const { title, type, placeholder, value, onChange, labelText } = props;
   return (
-    <div className="flex flex-col gap-[8px] w-full font-cairo">
+    <div className="relative flex flex-col gap-[8px] w-full font-cairo">
       <h1 className="font-[#151515] font-bold">{title}</h1>
+      <label className=" text-xs-regular top-0 left-3 px-1 bg-white absolute text-neutral-500">
+        {labelText}
+      </label>
       <input
         type={type}
         placeholder={placeholder}
