5b3907b1b447e42a306c175405805bba11a33b62
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-06-03 01:09:49 +0800

    feat:add step

--- src/pages/admin/vacancy/edit/index.jsx
+++ src/pages/admin/vacancy/edit/index.jsx
@@ -1,20 +1,35 @@
 import { useParams } from "react-router-dom";
-import { vacancies } from "data/vacancy";
+import { useState } from "react";
 
 import AdminNavbar from "components/organisms/AdminNavbar";
-import EditContainer from "components/organisms/EditContainer";
-import EditInput from "components/molecules/EditInput";
-import InputField from "components/atoms/InputField";
-import Select from "components/atoms/Select";
+
+import { vacancies } from "data/vacancy";
 
 import Chevron from "assets/icons/chevron-down.svg";
 import Ingfo from "assets/icons/Ingfo.svg";
 import NavigateNext from "assets/icons/navigate_next.svg";
 import NavigateBefore from "assets/icons/navigate_before.svg";
+import Informasi from "./Informasi";
+import Ketentuan from "./Ketentuan";
 
 export default function Edit() {
   const { id } = useParams();
+
+  const [step, setStep] = useState(1);
+  const [onConfirm, setOnConfirm] = useState(false);
+
   const vacancy = vacancies.find((loker) => loker.id === id);
+
+  const handlePrev = () => {
+    setStep(Math.max(step - 1, 1));
+  };
+
+  const handleNext = () => {
+    setStep(Math.min(step + 1, 2));
+    if (step === 2) {
+      setOnConfirm(true);
+    }
+  };
 
   return (
     <div className="flex w-full flex-col flex-1 pb-[63px] pr-[30px] lg:pr-[100px] pl-[30px] text-black">
@@ -43,101 +58,23 @@ export default function Edit() {
         </div>
       </section>
 
-      <section className="flex flex-col gap-10 mt-[76px]">
-        <EditContainer title="Informasi Lowongan">
-          <EditInput
-            isLabeled={true}
-            title="Nama pekerjaan"
-            desc="Buatlah nama pekerjaan sesuai dengan kata kunci yang akan digunakan."
-            fit={true}
-          >
-            <InputField placeholder="Nama pekerjaan" />
-          </EditInput>
-
-          <EditInput
-            title="Kebijakan bekerja"
-            desc="Infokan terkait kebijakan bekerja yang diberlakukan pada pekerjaan ini. Bisa berupa kerja dari kantor (WFO), kerja dari mana pun (WFA), atau campuran keduanya (Hybrid)."
-            fit={true}
-          >
-            <Select
-              isLabeled={true}
-              title="Kebijakan bekerja"
-              data={["WFO", "WFA", "Hybrid"]}
-              width="w-[250px]"
-            />
-          </EditInput>
-
-          <EditInput
-            title="Lokasi penempatan"
-            desc="Buatlah nama pekerjaan sesuai dengan kata kunci yang akan digunakan."
-          >
-            <div className="flex gap-10 ">
-              <Select
-                isLabeled={true}
-                title="Provinsi"
-                data={["Jawa Barat", "Jawa Tengah", "Jawa Timur", "Jawa Utara"]}
-              />
-              <Select
-                isLabeled={true}
-                title="Kota"
-                data={["Bandung", "Semarang", "Surabaya"]}
-              />
-              <Select
-                isLabeled={true}
-                title="Kecamatan"
-                data={["Bandung", "Semarang", "Surabaya"]}
-              />
-            </div>
-          </EditInput>
-
-          <EditInput
-            title="Hari kerja"
-            desc="Pilihlah hari kerja dibawah ini, sesuai ketentuan perusahaan Anda."
-          >
-            tes tes
-          </EditInput>
-
-          <EditInput title="Tipe Pekerjaan">
-            <div className="flex gap-10 ">
-              <Select
-                isLabeled={true}
-                title="Bidang pekerjaan"
-                data={["Jawa Barat", "Jawa Tengah", "Jawa Timur", "Jawa Utara"]}
-              />
-              <Select
-                isLabeled={true}
-                title="Level pekerjaan"
-                data={["Bandung", "Semarang", "Surabaya"]}
-              />
-              <Select
-                isLabeled={true}
-                title="Tipe pekerjaan"
-                data={["Bandung", "Semarang", "Surabaya"]}
-              />
-            </div>
-          </EditInput>
-        </EditContainer>
-
-        <EditContainer title="Detail Lowongan">
-          <EditInput
-            title="Benefit/Tunjangan pekerjaan"
-            desc="Benefit/Tunjangan adalah komponen di luar gaji yang mempengaruhi keputusan pencari kerja saat melamar. Isilah sesuai ketentuan perusahaan Anda"
-          >
-            <Select
-              isLabeled={true}
-              title="Benefit pekerjaan"
-              data={["WFO", "WFA", "Hybrid"]}
-            />
-          </EditInput>
-        </EditContainer>
-      </section>
+      {step === 1 ? <Informasi /> : <Ketentuan />}
 
       <section className="flex justify-between mt-12">
-        <button className="flex items-center py-2 pl-5 pr-4 gap-3 text-neutral-500 button-m-regular rounded-xl bg-neutral-300">
+        <button
+          onClick={handlePrev}
+          className={`flex items-center py-2 pl-5 pr-4 gap-3 text-neutral-500 button-m-regular rounded-xl ${
+            step === 1 ? "bg-neutral-200" : "bg-neutral-300"
+          }`}
+          disabled={step === 1}
+        >
           <img src={NavigateBefore} alt="Navigate before" />
           <p className="w-[88px] text-left">Kembali</p>
         </button>
-        <button className="flex items-center py-2 pl-4 pr-5 gap-3 text-neutral-100 button-m-regular rounded-xl bg-secondary-yellow drop-shadow-[0_0_12px_rgba(255,174,95,0.5)]">
+        <button
+          onClick={handleNext}
+          className="flex items-center py-2 pl-4 pr-5 gap-3 text-neutral-100 button-m-regular rounded-xl bg-secondary-yellow drop-shadow-[0_0_12px_rgba(255,174,95,0.5)]"
+        >
           <p className="w-[88px] text-left">Selanjutnya</p>
           <img src={NavigateNext} alt="Navigate next" />
         </button>
