b763c1b2132019ebaa38233619b8ac9768618c83
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-06-03 01:26:51 +0800

    feat:dynamic width, apply ds

--- src/components/atoms/InputField.jsx
+++ src/components/atoms/InputField.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 export default function InputField(props) {
-  const { title, type, placeholder, value, onChange, labelText } = props;
+  const { title, type, placeholder, value, onChange, labelText, width } = props;
   return (
-    <div className="relative flex flex-col gap-[8px] w-full font-cairo">
+    <div
+      className={`relative flex flex-col gap-[8px] w-full ${width} font-cairo`}
+    >
       <h1 className="font-[#151515] font-bold">{title}</h1>
       <label className=" text-xs-regular top-0 left-3 px-1 bg-white absolute text-neutral-500">
         {labelText}
@@ -13,7 +15,7 @@ export default function InputField(props) {
         placeholder={placeholder}
         value={value}
         onChange={onChange}
-        className="w-full px-[16px] py-[10px] rounded-[8px] border-[1px] border-primaryNavy-border focus:outline-none focus:border-primaryNavy-focus "
+        className="w-full px-[16px] py-[10px] rounded-[8px] border-[1px] button-s-regular border-primaryNavy-border focus:outline-none focus:border-primaryNavy-focus "
       />
     </div>
   );
