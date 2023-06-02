9f667bb52c8fc0503c9be263765e6128ac8a42f4
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-06-02 23:06:29 +0800

    feat:dynamic title

--- src/components/organisms/EditContainer.jsx
+++ src/components/organisms/EditContainer.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 export default function EditContainer(props) {
-  const { children } = props;
+  const { children, title } = props;
   return (
     <section className="mt-[76px] flex flex-col gap-7 drop-shadow-[0_0_4px_rgba(0,0,0,0.25)] bg-white rounded-xl p-10 w-full">
-      <h1 className="mb-3 heading-m-bold">Informasi Lowongan</h1>
+      <h1 className="mb-3 heading-m-bold">{title}</h1>
       { children }
     </section>
   );
