4b907e5ae717e18440c437a0a3f34eae04c6e9ba
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-06-02 23:00:20 +0800

    fix select label

--- src/components/atoms/Select.jsx
+++ src/components/atoms/Select.jsx
@@ -20,19 +20,21 @@ export default function Select(props) {
 
   return (
     <div
-      className={`box-border relative text-neutral-1000  rounded-[8px] flex flex-col flex-1 gap-3 w-fit max-h-[216px]  ${
+      className={`box-border text-neutral-1000  rounded-[8px] flex flex-col flex-1 gap-3 w-fit max-h-[216px]  ${
         isOpen &&
         "z-50 -mx-2 -my-2 py-2 px-2 border-[1px] border-neutral-300 h-full  shadow-[2px_4px_10px_rgba(19,47,159,0.25)]"
       } bg-white`}
     >
-      {isLabeled && (
-        <label className=" text-xs-regular -top-2 left-4 px-1 bg-white absolute">
-          {title}
-        </label>
-      )}
       <div
-        className={`w-full ${fit && "w-fit"} ${width} border-primaryNavy-border border-[1px] py-3 pl-4 pr-5 rounded-lg flex items-center`}
+        className={`relative w-full ${
+          fit && "w-fit"
+        } ${width} border-primaryNavy-border border-[1px] py-3 pl-4 pr-5 rounded-lg flex items-center`}
       >
+        {isLabeled && (
+          <label className=" text-xs-regular -top-2 left-4 px-1 bg-white absolute text-neutral-500">
+            {title}
+          </label>
+        )}
         <button
           onClick={handleClick}
           className="border-none justify-between focus:outline-none flex gap-2 items-center flex-1"
