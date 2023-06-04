60c21b81ae5c9a86980a58ee6fea18c1c9e52894
azkifairuz <azkiajmal@gmail.com>
2023-06-04 17:03:21 +0700

    create handle card open

--- src/components/molecules/PopUpSort.jsx
+++ src/components/molecules/PopUpSort.jsx
@@ -3,18 +3,18 @@ import { X } from "react-bootstrap-icons";
 export default function PopUpSort (){
     return(
         <>
-            <div className="w-[300px]  flex-col h-[160px] rounded-[40px] flex">
-                <div className="self-end">
-                    <X size={13.18} color="#323232"/>
+            <div className="w-[300px] border    flex-col h-[160px] rounded-[40px] flex">
+                <div className="self-end flex top-[21px] right-[41px] relative  content-start cursor-pointer">
+                    <X size={30} color="#323232"/>
                 </div>
-                <div className="flex gap-[44px]">
+                <div className="flex ml-[40px] flex-col mt-[11px] gap-[20px]">
                     <h1 className="text-l-bold">Urutkan</h1>
-                    <div>
-                        <label className="flex gap-[8px]" htmlFor="terbaru">
+                    <div className="flex gap-[44px]">
+                        <label className="flex gap-[8px] text-s-regular" htmlFor="terbaru">
                             <input type="checkbox" />
                             Terbaru
                         </label>
-                        <label className="flex gap-[8px]" htmlFor="terbaru">
+                        <label className="flex gap-[8px] text-s-regular" htmlFor="terbaru">
                             <input type="checkbox" />
                             Terlama
                         </label>
--- src/pages/admin/applicants.jsx
+++ src/pages/admin/applicants.jsx
@@ -11,12 +11,16 @@ import filter from "assets/icons/filter-bold.svg"
 
 import { applicants } from "data/applicants";
 import AdminNavbar from "components/organisms/AdminNavbar";
+import PopUpSort from "components/molecules/PopUpSort";
 
 export default function Applicants() {
   const [applicantsArr, setApplicantsArr] = useState(applicants[0].data);
   const [filterStatus, setFilterStatus] = useState("all");
   const maxData = 100;
-
+  const [hidenSort,setHidenSort] = useState("hidden")
+  const handleCardSort = () =>{
+    setHidenSort(!hidenSort)
+  }
   const applicantStatus = [
     {
       title: "Belum Diproses",
@@ -39,12 +43,12 @@ export default function Applicants() {
       count: 40,
     },
   ];
-
+  
   return (
     <main className="flex-1 pb-[35px] pr-[30px] lg:pr-[100px] pl-[30px] flex flex-col">
       <AdminNavbar />
       <h1 className="heading-l-bold ">Kelola Lowongan</h1>
-      <section className="flex gap-10 mt-4">
+      <section  className="flex gap-10 mt-4">
         {applicantStatus.map((item, index) => (
           <ApplicantsTab
             key={index}
@@ -55,6 +59,9 @@ export default function Applicants() {
           />
         ))}
       </section>
+      <section className={`${hidenSort && "hidden"} block`}>
+          <PopUpSort/>
+      </section>
       <section className="flex gap-6 mt-6 w-full first:w-3/4 justify-between last:w-fit ">
         <SearchBar
           placeholder="Cari lowongan"
@@ -62,11 +69,11 @@ export default function Applicants() {
           width="w-[60%]"
         />
       <div className="flex gap-[50px] cursor-pointer">
-        <div className="flex gap-[11px] items-center">
+        <div  className="flex gap-[11px] items-center">
             <img src={filter} alt="filter" />
             <h1 className="text-xl-bold">Filter</h1>
           </div>
-          <div className="flex gap-[11px] items-center cursor-pointer">
+          <div onClick={handleCardSort} className="flex gap-[11px] items-center cursor-pointer">
             <img src={sort} alt="sort" />
             <h1 className="text-xl-bold">Urutkat Pelamar</h1>
           </div>
