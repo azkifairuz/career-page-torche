ba2e8adb9ef3e46885c29304cda0516c1e6b24a3
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-06-09 01:51:57 +0800

    fix:title shrinked

--- src/components/organisms/EditContainer.jsx
+++ src/components/organisms/EditContainer.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 export default function EditContainer(props) {
-  const { children, title } = props;
+  const { children, title = "" } = props;
   return (
     <section className="flex flex-col gap-7 drop-shadow-[0_0_4px_rgba(0,0,0,0.25)] bg-white rounded-xl p-10 w-full">
-      <h1 className="mb-3 heading-m-bold">{title}</h1>
-      { children }
+      {title && <h1 className="mb-3 heading-m-bold">{title}</h1>}
+      {children}
     </section>
   );
 }
