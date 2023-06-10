2935955c8ea9adc71da055a6e7a590e83bdc1229
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-06-10 19:51:15 +0800

    fix:unnecessary css rule

--- src/components/organisms/EditContainer.jsx
+++ src/components/organisms/EditContainer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function EditContainer(props) {
   const { children, title = "" } = props;
   return (
-    <section className="z-0 flex flex-col gap-7 drop-shadow-[0_0_4px_rgba(0,0,0,0.25)] bg-white rounded-xl p-10 w-full">
+    <section className="flex flex-col gap-7 drop-shadow-[0_0_4px_rgba(0,0,0,0.25)] bg-white rounded-xl p-10 w-full">
       {title && <h1 className="mb-3 heading-m-bold">{title}</h1>}
       {children}
     </section>
