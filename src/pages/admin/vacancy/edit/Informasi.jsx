e65aa1ed23caff2c47c8e01fa07d1ba9b197124d
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-06-10 19:50:15 +0800

    fix:reverse order for z index

--- src/pages/admin/vacancy/edit/Informasi.jsx
+++ src/pages/admin/vacancy/edit/Informasi.jsx
@@ -18,9 +18,40 @@ export default function Informasi() {
     { day: "Minggu", id: 7 },
   ];
   return (
-    <section className="flex flex-col gap-10 mt-[76px]">
-      <EditContainer title="Informasi Lowongan">
+    <section className="flex flex-col-reverse gap-10 mt-[76px]">
+      <EditContainer title="Detail Lowongan">
+        <section className="flex flex-col gap-9">
+          <h1 className="heading-s-bold">
+            Deskripsi pekerjaan, kualifikasi, dan benefit karyawan
+          </h1>
+          <textarea
+            placeholder="Saran: Agar lebih mudah dibaca, jelaskan poin-poin yang ingin Anda sampaikan dengan bullet points atau angka. Contohnya:
+            &#10;
+Deskripsi pekerjaan: 
+1. Melakukan user research berupa user persona, user journey
+2. Membuat wireframe dan menjadikannya hi-fi
+3. Melakukan usability testing
+&#10;
+Kualifikasi yang dibutuhkan:
+1. Pengalaman kerja minimal 1 tahun
+2. Kemampuan menggunakan figma, adobe xd
+3. Memahami html, css, dan java script akan menjadi poin plus "
+            className="resize-none focus:outline-none p-4 pb-10 border-primaryNavy-border border-[1px] rounded-b-lg min-h-[392px]"
+          />
+        </section>
+        <EditInput
+          title="Benefit/Tunjangan pekerjaan"
+          desc="Benefit/Tunjangan adalah komponen di luar gaji yang mempengaruhi keputusan pencari kerja saat melamar. Isilah sesuai ketentuan perusahaan Anda"
+        >
+          <Select
+            isLabeled={true}
+            title="Benefit pekerjaan"
+            data={["WFO", "WFA", "Hybrid"]}
+          />
+        </EditInput>
+      </EditContainer>
 
+      <EditContainer title="Informasi Lowongan">
         <EditInput
           isLabeled={true}
           title="Nama pekerjaan"
@@ -35,14 +66,12 @@ export default function Informasi() {
           desc="Infokan terkait kebijakan bekerja yang diberlakukan pada pekerjaan ini. Bisa berupa kerja dari kantor (WFO), kerja dari mana pun (WFA), atau campuran keduanya (Hybrid)."
           fit={true}
         >
-          <div className="relative min-h-[42px] h-fit overflow-visible">
-            <Select
-              isLabeled={true}
-              title="Kebijakan bekerja"
-              data={["WFO", "WFA", "Hybrid"]}
-              width="w-[250px]"
-            />
-          </div>
+          <Select
+            isLabeled={true}
+            title="Kebijakan bekerja"
+            data={["WFO", "WFA", "Hybrid"]}
+            width="w-[250px]"
+          />
         </EditInput>
 
         <EditInput
@@ -110,38 +139,6 @@ export default function Informasi() {
               data={["Bandung", "Semarang", "Surabaya"]}
             />
           </div>
-        </EditInput>
-      </EditContainer>
-
-      <EditContainer title="Detail Lowongan">
-        <section className="flex flex-col gap-9">
-          <h1 className="heading-s-bold">
-            Deskripsi pekerjaan, kualifikasi, dan benefit karyawan
-          </h1>
-          <textarea
-            placeholder="Saran: Agar lebih mudah dibaca, jelaskan poin-poin yang ingin Anda sampaikan dengan bullet points atau angka. Contohnya:
-            &#10;
-Deskripsi pekerjaan: 
-1. Melakukan user research berupa user persona, user journey
-2. Membuat wireframe dan menjadikannya hi-fi
-3. Melakukan usability testing
-&#10;
-Kualifikasi yang dibutuhkan:
-1. Pengalaman kerja minimal 1 tahun
-2. Kemampuan menggunakan figma, adobe xd
-3. Memahami html, css, dan java script akan menjadi poin plus "
-            className="resize-none focus:outline-none p-4 pb-10 border-primaryNavy-border border-[1px] rounded-b-lg min-h-[392px]"
-          ></textarea>
-        </section>
-        <EditInput
-          title="Benefit/Tunjangan pekerjaan"
-          desc="Benefit/Tunjangan adalah komponen di luar gaji yang mempengaruhi keputusan pencari kerja saat melamar. Isilah sesuai ketentuan perusahaan Anda"
-        >
-          <Select
-            isLabeled={true}
-            title="Benefit pekerjaan"
-            data={["WFO", "WFA", "Hybrid"]}
-          />
         </EditInput>
       </EditContainer>
     </section>
