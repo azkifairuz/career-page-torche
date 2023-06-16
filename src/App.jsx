7481f99d7b14aad84dbeae57418caad86fcf9549
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-06-17 00:56:49 +0800

    remove auth context

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,6 @@ import Admin, {
 } from "pages/admin";
 import CompleteProfil from "./pages/complete profile/CompleteProfile";
 import UserDashboard from "pages/user/dashboard";
-import { AuthProvider } from "context";
 import Profil from "pages/user/dashboard/profil";
 import JobDetail from "pages/joblist/detail";
 import Tracking from "./pages/user/dashboard/application track";
@@ -42,85 +41,74 @@ function App() {
     roles: ["admin", "user"],
   });
 
-  const handleLogin = () =>
-    setUser({
-      id: "test-1",
-      name: "test",
-      roles: ["admin"],
-    });
-
-  const handleLogout = () => setUser(null);
-
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <ScrollToTop />
-        <Routes>
-          <Route path="login" element={<Login />} />
-          <Route path="signup">
-            <Route index element={<Signup />} />
-            <Route path="verify" element={<Verify />} />
-            {/* <Route path="success" element={<SignupSuccess />} /> */}
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="signup">
+          <Route index element={<Signup />} />
+          <Route path="verify" element={<Verify />} />
+          {/* <Route path="success" element={<SignupSuccess />} /> */}
+        </Route>
+        <Route path="forget-password">
+          <Route index element={<ForgetPassword />} />
+          <Route path="confirmed" element={<ChangePassword />} />
+        </Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Landing />} />
+          <Route path="joblist" element={<Joblist />} />
+          <Route path="about" element={<About />} />
+          <Route path="FAQ" element={<FAQ />} />
+          <Route path="debug" element={<CompleteProfil />} />
+        </Route>
+        <Route path={"/joblist/:id"} element={<JobDetail />} />
+        <Route
+          path="admin"
+          element={
+            <ProtectedRoute
+              redirectPath="/home"
+              isAllowed={!!user && user.roles.includes("admin")}
+            >
+              <Admin />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Navigate to="dashboard" />} />
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="vacancy" element={<Vacancy />} />
+          <Route path="applicants" element={<Applicants />} />
+          <Route path="vacancy/create" element={<Create />} />
+          <Route path="vacancy/:id/edit" element={<Edit />} />
+        </Route>
+        <Route
+          path="user"
+          element={
+            <ProtectedRoute
+              redirectPath="/login"
+              isAllowed={!!user && user.roles.includes("user")}
+            >
+              <User />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Navigate to="home" />} />
+          <Route path="home" element={<Landing />} />
+          <Route path="dashboard" element={<UserDashboard />}>
+            <Route index element={<Navigate to="profil" />} />
+            <Route path="profil" element={<Profil />}></Route>
+            <Route path="lamaran" element={<JobApplication />}></Route>
+            <Route path="applicationtracking" element={<Tracking />} />
+            <Route path="setting" element={<Setting />} />
           </Route>
-          <Route path="forget-password">
-            <Route index element={<ForgetPassword />} />
-            <Route path="confirmed" element={<ChangePassword />} />
-          </Route>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Landing />} />
-            <Route path="joblist" element={<Joblist />} />
-            <Route path="about" element={<About />} />
-            <Route path="FAQ" element={<FAQ />} />
-            <Route path="debug" element={<CompleteProfil />} />
-          </Route>
-          <Route path={"/joblist/:id"} element={<JobDetail />} />
-          <Route
-            path="admin"
-            element={
-              <ProtectedRoute
-                redirectPath="/home"
-                isAllowed={!!user && user.roles.includes("admin")}
-              >
-                <Admin />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<Navigate to="dashboard" />} />
-            <Route path="dashboard" element={<AdminDashboard />} />
-            <Route path="vacancy" element={<Vacancy />} />
-            <Route path="applicants" element={<Applicants />} />
-            <Route path="vacancy/create" element={<Create />} />
-            <Route path="vacancy/:id/edit" element={<Edit />} />
-          </Route>
-          <Route
-            path="user"
-            element={
-              <ProtectedRoute
-                redirectPath="/login"
-                isAllowed={!!user && user.roles.includes("user")}
-              >
-                <User />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<Navigate to="home" />} />
-            <Route path="home" element={<Landing />} />
-            <Route path="dashboard" element={<UserDashboard />}>
-              <Route index element={<Navigate to="profil" />} />
-              <Route path="profil" element={<Profil />}></Route>
-              <Route path="lamaran" element={<JobApplication />}></Route>
-              <Route path="applicationtracking" element={<Tracking />} />
-              <Route path="setting" element={<Setting />} />
-            </Route>
-            <Route path="FAQ" element={<FAQ />} />
-            <Route path="completeprofile" element={<CompleteProfil />}></Route>
-            <Route path="joblist" element={<Joblist />} />
-            <Route path="about" element={<About />} />
-          </Route>
-          <Route path="*" element={<Custom404 />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+          <Route path="FAQ" element={<FAQ />} />
+          <Route path="completeprofile" element={<CompleteProfil />}></Route>
+          <Route path="joblist" element={<Joblist />} />
+          <Route path="about" element={<About />} />
+        </Route>
+        <Route path="*" element={<Custom404 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/context/index.js
+++
@@ -1,4 +0,0 @@
-import { useAuthDispatch, useAuthState, AuthProvider } from "./AuthContext";
-import { login, logout } from "./actions";
-
-export { useAuthDispatch, useAuthState, AuthProvider, login, logout };
