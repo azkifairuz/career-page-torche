cfe8fde8b5f2a3857be852b2dffbfe39e76a4833
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-06-18 09:37:13 +0800

    refaactor name to button

---
+++ src/components/organisms/AdminSidebar/Button.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+
+import { Link } from "react-router-dom";
+
+export default function Button(props) {
+  const { src, title, href } = props;
+  return (
+    <Link
+      to={href}
+      className="flex gap-[20px] items-center px-4 py-3 hover:bg-neutral-300 rounded-lg"
+    >
+      <img src={src} alt={title} className="w-[22px]" />
+      <p className="heading-s-medium">{title}</p>
+    </Link>
+  );
+}
+
+Button.propTypes = {
+  src: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired,
+};
