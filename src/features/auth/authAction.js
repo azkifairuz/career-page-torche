c339d6f49b94983ccd9db4478840932d4e1f7457
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-06-24 14:59:22 +0800

    feat:reject if error

--- src/features/auth/authAction.js
+++ src/features/auth/authAction.js
@@ -1,23 +1,49 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { login } from "service/auth";
+import { login, signup } from "service/auth";
 import { getUserProfile } from "service/user";
 
-export const loginUser = createAsyncThunk("auth/login", async (data) => {
-  try {
-    const res = await login(data);
+export const loginUser = createAsyncThunk(
+  "auth/login",
+  async (data, { rejectWithValue }) => {
+    try {
+      const res = await login(data);
 
-    if (!res.success) {
-      throw new Error(res.messages ? res.messages[0] : res.message);
+      if (!res.success) {
+        throw new Error(
+          res.messages ? JSON.stringify(res.messages) : res.message
+        );
+      }
+
+      const userProfile = await getUserProfile();
+
+      if (!userProfile.success) {
+        throw new Error(userProfile.message);
+      }
+
+      return userProfile.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
     }
-
-    const userProfile = await getUserProfile();
-
-    if (!userProfile.success) {
-      throw new Error(userProfile.message);
-    }
-
-    return userProfile.data;
-  } catch (error) {
-    return error.message;
   }
-});
+);
+
+export const signupUser = createAsyncThunk(
+  "auth/signup",
+  async (data, { rejectWithValue }) => {
+    try {
+      const res = await signup(data);
+
+      if (!res.success) {
+        throw new Error(
+          res.messages ? JSON.stringify(res.messages) : res.message
+        );
+      }
+
+      console.log(res);
+
+      return res.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
