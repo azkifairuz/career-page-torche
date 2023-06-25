7259169121c3bc38372f7d026aa84d79a8af15d4
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-06-25 23:16:37 +0800

    fix:type and unnecesary function

--- src/components/atoms/PasswordInput.jsx
+++ src/components/atoms/PasswordInput.jsx
@@ -5,25 +5,20 @@ import { Eye, EyeSlash } from "react-bootstrap-icons";
 
 export default function PasswordInput(props) {
   const [isVisible, setIsVisible] = useState(false);
-  const { title, type, placeholder, value, onChange } = props;
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    setIsVisible(!isVisible);
-  };
+  const { title, placeholder, value, onChange } = props;
 
   return (
     <div className="flex flex-col gap-[8px] w-full font-cairo">
       <h1 className="font-[#151515] font-bold">{title}</h1>
-      <div className="flex w-full px-[16px] py-[10px] rounded-[8px] border-[1px] border-primaryNavy-border focus:outline-none focus:border-primaryNavy-focus ">
+      <div className="flex w-full px-[16px] py-[10px] rounded-[8px] border-[1px] border-primaryNavy-border hover:border-primaryBlue-hover focus-within:border-primaryBlue-main">
         <input
-          type={isVisible ? "text" : type}
+          type={isVisible ? "text" : "password"}
           placeholder={placeholder}
           value={value}
           onChange={onChange}
           className="focus:outline-none w-full active:bg-none autofill:bg-none"
         />
-        <button onClick={handleClick} type="button">
+        <button onClick={() => setIsVisible(!isVisible)} type="button">
           {isVisible ? <EyeSlash size={20} /> : <Eye size={20} />}
         </button>
       </div>
@@ -33,7 +28,6 @@ export default function PasswordInput(props) {
 
 PasswordInput.propTypes = {
   title: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
   placeholder: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
